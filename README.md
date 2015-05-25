47aa2a18db3f28fa6a6dda4b084fcba6178e428f
Miodrag Milic <miodrag.milic@gmail.com>
2015-05-25 10:44:35 +0200

    copyq

---
+++ README.md
@@ -0,0 +1,3 @@
+My chocolatey packages:
+
+https://chocolatey.org/profiles/majkinetor
---
+++ copyq/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'CopyQ'
+$installerType = 'EXE' #only one of these: exe, msi, msu
+$url = 'https://github.com/hluk/CopyQ/releases/download/v2.4.6/copyq-2.4.6-setup.exe' # download url
+#$url64 = 'URL_x64_HERE' # 64bit URL here or remove - if installer decides, then use $url
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
