83af69e7a84d4ec5221c50f79afded2f5daf1c11
Miodrag Milic <miodrag.milic@gmail.com>
2015-05-25 13:42:19 +0200

    smal fix

--- copyq/tools/chocolateyInstall.ps1
+++ copyq/tools/chocolateyInstall.ps1
@@ -1,4 +1,5 @@
 ﻿$packageName    = 'copyq'
+$programName    = 'copyq'
 $installerType  = 'exe'
 $url            = 'https://github.com/hluk/CopyQ/releases/download/v2.4.6/copyq-2.4.6-setup.exe' # download url
 $silentArgs     = '/VERYSILENT'
@@ -7,7 +8,7 @@ $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
 
 $key = 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
-$reg = ls $key | ? { (gp $_.PSPath DisplayName -ea ig) -match 'copyq'}
+$reg = ls $key | ? { (gp $_.PSPath DisplayName -ea ig) -match $programName}
 $installLocation = $reg.GetValue("InstallLocation")
 if (Test-Path $installLocation)  { Install-ChocolateyPath $installLocation }
 
--- copyq/tools/chocolateyUninstall.ps1
+++ copyq/tools/chocolateyUninstall.ps1
@@ -4,6 +4,6 @@ $fileType    = 'exe'
 $silentArgs  = '/VERYSILENT'
 
 $key = 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
-$reg = ls $key | ? { (gp $_.PSPath DisplayName -ea ig) -match 'copyq'}
+$reg = ls $key | ? { (gp $_.PSPath DisplayName -ea ig) -match $programName}
 $uninstallString = $reg.GetValue("UninstallString")
 Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $UninstallString
