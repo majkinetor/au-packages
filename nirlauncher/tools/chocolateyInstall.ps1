3de15ce26bed4a3c6487d566e88aa9f4398190c5
Miodrag Milic <miodrag.milic@gmail.com>
2015-06-14 22:57:53 +0200

    Nirlauncher moderation related changes

--- nirlauncher/tools/chocolateyInstall.ps1
+++ nirlauncher/tools/chocolateyInstall.ps1
@@ -1,33 +1,15 @@
-﻿# Without the Referer header set to downloads page Get-ChocolateyWebFile will fail
-#  and since it doesn't allow setting headers in parameters I need custom downloader
-$url = "http://launcher.nirsoft.net/download.html"
-$client = New-Object System.Net.WebClient
-if (!$client.Proxy.IsBypassed($url)) {
-    $creds = [Net.CredentialCache]::DefaultCredentials
-    $proxyAddress = $client.Proxy.GetProxy($url).Authority
-    Write-Host "Using this proxyserver: $proxyAddress"
-}
-
-$params = @{
-    Uri     = $url
-    Headers = @{ Referer = 'http://launcher.nirsoft.net/download.html' }
-}
-if ($proxyAddress) {$params.Proxy = $proxyAddress }
-if ($creds -and !($creds.Username -eq '')) { $params.ProxyCredential = $creds }
-
-$packageName   = 'nirlauncher'
-$download_page = Invoke-WebRequest @params
-$url           = $download_page.links | ? href -match "nirsoft_package_.*.zip" | select -First 1 -expand href
+﻿$packageName   = 'nirlauncher.portable'
+$url           = 'http://download.nirsoft.net/nirsoft_package_1.19.37.zip'
 $version       = $url -split '_|.zip' | select -Last 1 -Skip 1
 $installDir    = Split-Path -parent $MyInvocation.MyCommand.Definition
 $zipPath       = "$installDir\$($packageName)_$version.zip"
 
-Write-Host "Downloading $packageName $version ..."
-$params.Uri = $url
-$params.OutFile = $zipPath
-Invoke-WebRequest @params
 
-Write-Host "Installing $packageName $version and creating shims"
+Write-Host "Downloading NirLauncher v$version ..."
+$client = New-Object System.Net.WebClient
+$client.Headers.Add('Referer','http://launcher.nirsoft.net/download.html')
+$client.DownloadFile($url, $zipPath)
+
 Write-Host "Local path: $installDir"
 Install-ChocolateyZipPackage $packageName $zipPath $installDir
 rm $zipPath
