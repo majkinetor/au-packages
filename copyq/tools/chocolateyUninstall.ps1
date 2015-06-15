fc978770719fc79b595e42492847d48b5b0d9e2c
Miodrag Milic <miodrag.milic@gmail.com>
2015-06-15 10:34:46 +0200

    nir, copyq fixes

--- copyq/tools/chocolateyUninstall.ps1
+++ copyq/tools/chocolateyUninstall.ps1
@@ -3,7 +3,12 @@ $programName = 'copyq'
 $fileType    = 'exe'
 $silentArgs  = '/VERYSILENT'
 
-$key = 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+$processor = Get-WmiObject Win32_Processor
+$is64bit = $processor.AddressWidth -eq 64
+
+if ($is64bit) { $key = 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall' }
+else          { $key = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall' }
+
 $reg = ls $key | ? { (gp $_.PSPath DisplayName -ea ig) -match $programName}
 $uninstallString = $reg.GetValue("UninstallString")
 Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $UninstallString
--- nirlauncher/tools/chocolateyInstall.ps1
+++ nirlauncher/tools/chocolateyInstall.ps1
@@ -1,15 +1,18 @@
 ﻿$packageName   = 'nirlauncher.portable'
 $url           = 'http://download.nirsoft.net/nirsoft_package_1.19.37.zip'
 $version       = $url -split '_|.zip' | select -Last 1 -Skip 1
-$installDir    = Split-Path -parent $MyInvocation.MyCommand.Definition
-$zipPath       = "$installDir\$($packageName)_$version.zip"
-
+$installDir    = "$env:ChocolateyBinRoot\NirLauncher"
+$downloadPath  = "$env:TEMP\$($packageName)_$version.zip"
 
 Write-Host "Downloading NirLauncher v$version ..."
 $client = New-Object System.Net.WebClient
 $client.Headers.Add('Referer','http://launcher.nirsoft.net/download.html')
-$client.DownloadFile($url, $zipPath)
+$client.DownloadFile($url, $downloadPath)
 
 Write-Host "Local path: $installDir"
-Install-ChocolateyZipPackage $packageName $zipPath $installDir
-rm $zipPath
+Install-ChocolateyZipPackage $packageName $downloadPath $installDir
+rm $downloadPath
+
+Write-Host "Adding NirLuancher utilities to the PATH"
+"$installDir","$installDir\Nirsoft" | % Install-ChocolateyPath $_ "Machine"
+
