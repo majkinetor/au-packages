bcfb707cac7f7cd5e03de70fb5618db6555a8867
Miodrag Milic <miodrag.milic@gmail.com>
2016-01-20 13:28:03 +0100

    dngrep update

--- dngrep/tools/chocolateyInstall.ps1
+++ dngrep/tools/chocolateyInstall.ps1
@@ -1,30 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
-$is64bit = Get-ProcessorBits -eq '64'
 
 $packageName = 'dngrep'
 $url64       = 'https://github.com/dnGrep/dnGrep/releases/download/v2.8.16.0/dnGREP.2.8.16.x64.msi'
 $url32       = 'https://github.com/dnGrep/dnGrep/releases/download/v2.8.16.0/dnGREP.2.8.16.x86.msi'
 
-$url         = $url64
-if (!$is64bit) { $url = $url32 }
-
 $packageArgs = @{
   packageName            = $packageName
   fileType               = 'MSI'
-  url                    = $url
+  url                    = $url32
+  url64bit               = $url64
   silentArgs             = '/quiet'
   validExitCodes         = @(0)
   registryUninstallerKey = $packageName
 }
 Install-ChocolateyPackage @packageArgs
-
-$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$key = Get-ItemProperty -Path @($machine_key6432, $machine_key, $local_key) -ErrorAction SilentlyContinue | ? { $_.DisplayName -like "$packageName*" }
-if ($key) {
-    $installLocation = $key.InstallLocation
-    if (Test-Path $installLocation)  {
-        Write-Host "$packageName installed to '$installLocation'"
-    }
-}
