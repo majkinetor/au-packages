87d7b7630548b60d3a93dde9cbde70469ed8c2fe
Miodrag Milic <miodrag.milic@gmail.com>
2016-01-20 21:34:53 +0100

    update

--- dngrep/update.ps1
+++ dngrep/update.ps1
@@ -1,18 +1,20 @@
+. ../au.ps1
+
 $releases = 'https://github.com/dnGrep/dnGrep/releases'
 
-function SearchReplace {
+function au_SearchReplace {
     @{
-        ".\tools\chocolateyInstall.ps1" = @{
+        'tools\chocolateyInstall.ps1' = @{
             "(^[$]url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
             "(^[$]url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
         }
      }
 }
 
-function GetLatest {
+function au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
-    $url      = $download_page.links | ? href -match "dnGREP.*.msi" | select -First 2 -expand href
-    $version  = $url -split '-|.exe' | select -Last 1 -Skip 2
+    $url      = $download_page.links | ? href -match 'dnGREP.*.msi' | select -First 2 -expand href
+    $version  = ($url[0] -split '\/' | select -Index 5).Substring(1)
 
     $url64    = 'https://github.com' + $url[0]
     $url32    = 'https://github.com' + $url[1]
@@ -21,3 +23,4 @@ function GetLatest {
     return $Latest
 }
 
+update
