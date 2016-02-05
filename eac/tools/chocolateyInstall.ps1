d899f0236a9fe6639d9f5d127f6bc832fe14e053
Miodrag Milic <miodrag.milic@gmail.com>
2016-02-05 12:15:43 +0100

    eac

---
+++ eac/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'eac'
+$url32       = 'http://www.exactaudiocopy.de/eac-1.1.exe'
+
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'EXE'
+  url                    = $url32
+  silentArgs             = '/S'
+  validExitCodes         = @(0)
+  registryUninstallerKey = "Exact Audio Copy"
+}
+Install-ChocolateyPackage @packageArgs
+
+$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$key = Get-ItemProperty -Path @($machine_key6432, $machine_key, $local_key) -ErrorAction SilentlyContinue | ? { $_.DisplayName -like "$($packageArgs.registryUninstallerKey)*" }
+if ($key) {
+    $installLocation = Split-Path $key.UninstallString
+    if (Test-Path $installLocation)  { Write-Host "$packageName installed to '$installLocation'" }
+}
+
