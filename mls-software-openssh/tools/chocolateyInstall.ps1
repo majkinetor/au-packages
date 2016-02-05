01f138a8f4082586306535a7b54336b220518861
Miodrag Milic <miodrag.milic@gmail.com>
2016-02-05 21:19:15 +0100

    name change

--- mls-software-openssh/tools/chocolateyInstall.ps1
+++ mls-software-openssh/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'openssh'
+$packageName = 'mls-software-openssh'
 $url         = 'http://www.mls-software.com/files/setupssh-7.1p2-1.exe'
 
 $packageArgs = @{
@@ -10,14 +10,14 @@ $packageArgs = @{
   url64Bit               = $url
   silentArgs             = "/S"
   validExitCodes         = @(0)
-  registryUninstallerKey = $packageName
+  registryUninstallerKey = "OpenSSH"
 }
 Install-ChocolateyPackage @packageArgs
 
 $local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
 $machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
 $machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$key = Get-ItemProperty -Path @($machine_key6432, $machine_key, $local_key) -ErrorAction SilentlyContinue | ? { $_.DisplayName -like "$packageName*" }
+$key = Get-ItemProperty -Path @($machine_key6432, $machine_key, $local_key) -ErrorAction SilentlyContinue | ? { $_.DisplayName -like "OpenSSH*" }
 if ($key) {
     $installLocation = Split-Path $key.UninstallString
     if (Test-Path $installLocation)  {
