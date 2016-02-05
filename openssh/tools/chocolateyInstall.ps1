073ee2696d8788ccb822749ea9646ed8ebf7adc0
Miodrag Milic <miodrag.milic@gmail.com>
2016-02-05 19:42:58 +0100

    removed parameters cos it didn't work...

--- openssh/tools/chocolateyInstall.ps1
+++ openssh/tools/chocolateyInstall.ps1
@@ -1,38 +1,4 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageParameters = $env:chocolateyPackageParameters
-
-$port = 22
-$password = 'root'
-
-if ($packageParameters) {
-    $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z]+))"
-    $option_name = 'option'
-    $value_name = 'value'
-    $arguments = @{}
-
-    if ($packageParameters -match $match_pattern ){
-        $results = $packageParameters | Select-String $match_pattern -AllMatches
-        $results.matches | % {
-        $arguments.Add(
-            $_.Groups[$option_name].Value.Trim(),
-            $_.Groups[$value_name].Value.Trim())
-    }
-    }
-    else
-    {
-        Throw "Package Parameters were found but were invalid (REGEX Failure)"
-    }
-
-    if ($arguments.ContainsKey("Port")) {
-        Write-Host "Port Argument Found"
-        $port = $arguments["Port"]
-    }
-
-    if ($arguments.ContainsKey("Password")) {
-        Write-Host "Password Argument Found"
-        $password = $arguments["Password"]
-    }
-}
 
 $packageName = 'openssh'
 $url         = 'http://www.mls-software.com/files/setupssh-7.1p2-1.exe'
@@ -42,8 +8,7 @@ $packageArgs = @{
   fileType               = 'EXE'
   url                    = $url
   url64Bit               = $url
-  #Start-Process "$temp\openssh.exe" "/S /port=22 /privsep=1 /password=$ssh_admin_pass" -NoNewWindow -Wait
-  silentArgs             = "/S /port=$port /privsep=1 /password=$password"
+  silentArgs             = "/S"
   validExitCodes         = @(0)
   registryUninstallerKey = $packageName
 }
