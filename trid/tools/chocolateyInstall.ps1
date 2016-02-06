cb8472d44e6868c45ed445937fc215860822a7c2
Miodrag Milic <miodrag.milic@gmail.com>
2016-02-06 17:25:37 +0100

    trid 2.20

---
+++ trid/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'trid'
+$url_trid    = 'http://goo.gl/aJVb'
+$url_trid_db = 'http://goo.gl/Bnw1'
+
+$packageArgs = @{
+  packageName   = $packageName
+  url           = $url_trid
+  #unzipLocation = "$ENV:ChocolateyInstall\bin"
+  unzipLocation = Split-Path $MyInvocation.MyCommand.Definition
+}
+Install-ChocolateyZipPackage @packageArgs
+$packageArgs.url = $url_trid_db
+Install-ChocolateyZipPackage @packageArgs
