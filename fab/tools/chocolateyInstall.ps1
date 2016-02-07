6be277a95dabee080cbaf87b22383f273d865a0b
Miodrag Milic <miodrag.milic@gmail.com>
2016-02-07 17:41:26 +0100

    fab and dnsjumper fix

--- fab/tools/chocolateyInstall.ps1
+++ fab/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName  = 'dnsjumper'
-$url          = 'http://sordum.org/files/dns-jumper/DnsJumper.zip'
+$packageName  = 'fab'
+$url          = 'http://sordum.org/files/firewall-app-blocker/fab_v1.4.zip'
 $chocoTempDir = Join-Path $Env:Temp "chocolatey"
 $toolsDir     = Split-Path $MyInvocation.MyCommand.Definition
-$tempZipFile  = "$chocoTempDir\dnsjumper.zip"
-$Checksum     = '6c83c19f796aa104e28948d70149ef406e1b9a98'
+$tempZipFile  = "$chocoTempDir\fab.zip"
+$Checksum     = '7762ff1fbd5cd16cf6679d97623543edbe758806'
 
 mkdir -ea 0 $chocoTempDir | out-null
 
 "Downloading $url ..."
-$referer = "http://www.sordum.org/7952/dns-jumper-v2-0"
+$referer = 'http://www.sordum.org/8125/firewall-app-blocker-fab-v1-4'
 $wc = New-Object System.Net.WebClient
 $wc.Headers.Add("Referer", $referer)
 $wc.DownloadFile($url, $tempZipFile)
@@ -18,5 +18,5 @@ $wc.DownloadFile($url, $tempZipFile)
 Get-ChocolateyUnzip -PackageName "$packageName" -FileFullPath "$tempZipFile" -Destination "$toolsDir"
 rm $tempZipFile -ea 0 -force | out-null
 
-$fileFullPath = $toolsDir +"\dnsjumper\dnsjumper.exe"
+$fileFullPath = $toolsDir +"\fab_v1.4\fab.exe"
 Get-CheckSumValid -file $fileFullPath -checkSum $checksum -checksumType 'sha1'
