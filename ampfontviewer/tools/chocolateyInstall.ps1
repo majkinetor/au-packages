5aa00aca9d33d40eb393d8653c47f551af506ab2
Miodrag Milic <miodrag.milic@gmail.com>
2016-02-07 11:21:43 +0100

    amp font viewer

---
+++ ampfontviewer/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'ampfontviewer'
+$url32       = 'http://www.ampsoft.net/files/FontViewer.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  url           = $url32
+  unzipLocation = Split-Path $MyInvocation.MyCommand.Definition
+}
+Install-ChocolateyZipPackage @packageArgs
