6637f314be905c3b19d74dc1ae40bae07b807151
Miodrag Milic <miodrag.milic@gmail.com>
2016-02-07 10:44:07 +0100

    ShadowSpawn 0.2.2

---
+++ shadowspawn/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$root = Split-Path -parent $MyInvocation.MyCommand.Definition
+$is64bit = Get-ProcessorBits -eq '64'
+
+"Installing $(Get-ProcessorBits) bit version"
+if ($is64bit) { rm -r -force $root\x86 } else { rm -r -force $root\x64 }
