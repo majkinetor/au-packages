5d8c40eea848d4bc8e6136caad02071e75163879
Miodrag Milic <miodrag.milic@gmail.com>
2016-02-27 15:49:24 +0100

    update

---
+++ nexus-oss/update.ps1
@@ -0,0 +1,25 @@
+import-module au
+
+$url = 'http://www.sonatype.org/downloads/nexus-latest-bundle.zip'
+
+function global:au_SearchReplace {
+    @{".\tools\chocolateyInstall.ps1" = @{ "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL)'" }}
+}
+
+function global:au_GetLatest {
+    while($true) {
+        Write-Host "Tryinig $url"
+        $request = [System.Net.WebRequest]::Create($url)
+        $request.AllowAutoRedirect=$false
+        $response=$request.GetResponse()
+        $location = $response.GetResponseHeader('Location')
+        if (!$location -or ($location -eq $url)) { break }
+        $url = $location
+    }
+
+    $version = ($url -split '-|\.' | select -Last 4 -skip 2) -join '.'
+    $Latest = @{ URL = $url; Version = $version }
+    return $Latest
+}
+
+update -NoCheckUrl
