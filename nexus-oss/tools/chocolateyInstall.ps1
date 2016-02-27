de4ff5bf9c7cb6dd5e3763b6d3a6c6fa6d65e3d9
Miodrag Milic <miodrag.milic@gmail.com>
2016-02-27 12:06:21 +0100

    nexus install

---
+++ nexus-oss/tools/chocolateyInstall.ps1
@@ -0,0 +1,51 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'nexus-oss'
+$url         = 'https://sonatype-download.global.ssl.fastly.net/nexus/oss/nexus-2.12.0-01-bundle.zip'
+$installDir  = 'c:\nexus'
+
+$packageArgs = @{
+  packageName   = $packageName
+  url           = $url
+  url64Bit      = $url
+  unzipLocation = $installDir
+}
+
+
+if ($Env:ChocolateyPackageParameters -match '/InstallDir:\s*(.+)') {
+    $installDir = $Matches[1]
+    if ($installDir.StartsWith("'") -or $installDir.StartsWith('"')){  $installDir = $installDir -replace '^.|.$' }
+    $parent = Split-Path $installDir
+    mkdir -force $parent -ea 0 | out-null
+}
+
+if (gcm nexus -ea 0) {
+    Write-Host "Detected existing installation, uninstalling service"
+    nexus stop
+    nexus uninstall
+}
+
+Write-Host "Installing to '$installDir'"
+Install-ChocolateyZipPackage @packageArgs
+
+$nexusDir = ls C:\nexus\nexus-* | sort -Descending | select -First 1
+$nexus = "$nexusDir\bin\nexus.bat"
+if (!(Test-Path $nexus)) { throw "Can not find nexus.bat" }
+Install-BinFile nexus $nexus
+
+0 | nexus install
+0 | nexus start
+
+$s = gsv nexus-webapp -ea 0
+if (!$s) { throw "Nexus service 'nexus-webapp' is not installed" }
+if ($s.Status -ne 'Running') { Write-Warning "Nexus service 'nexus-webapp' is installed but not running" }
+
+$ok = $false
+try {
+    $request  = [System.Net.HttpWebRequest]::Create("http://localhost:8081/nexus")
+    $request.GetResponse()
+    $ok = $request.StatusCode -eq 'OK'
+} catch {}
+
+if (!$ok) { Write-Warning "Nexus should be available at http://localhost:8081/nexus but can't be reached" }
+Write-Host "Nexus is available at http://localhost:8081/nexus"
