3ab570e5a3ad8af87d3473a096d5032790c563ca
Miodrag Milic <miodrag.milic@gmail.com>
2016-03-12 16:03:01 +0100

    template update

---
+++ _template/tools/chocolateyInstallZip.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = ''
+$url32 = ''
+$url64 = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  url           = $url32
+  url64Bit      = $url64
+  unzipLocation = Split-Path $MyInvocation.MyCommand.Definition
+}
+Install-ChocolateyZipPackage @packageArgs
--- _template/update.ps1
+++ _template/update.ps1
@@ -1,12 +1,12 @@
-. ../au.ps1
+import-module au
 
 $releases = ''
 
-function au_SearchReplace {
+function global:au_SearchReplace {
     @{".\tools\chocolateyInstall.ps1" = @{ "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL)'" }}
 }
 
-function au_GetLatest {
+function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
     $re    = ''
