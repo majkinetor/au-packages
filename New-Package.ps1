e88b9ffd41aaf368bfc3817e9d80620fa48b05ff
Miodrag Milic <miodrag.milic@gmail.com>
2016-03-12 16:36:07 +0100

    new-package from template

---
+++ New-Package.ps1
@@ -0,0 +1,25 @@
+param($Name)
+
+<#
+.SYNOPSIS
+    Create a new package from the template
+
+.DESCRIPTION
+    This function creates a new package by copying the directory _template which contains desired package basic settings.
+#>
+function New-Package( $Name ) {
+    if ($Name -eq $null) { throw "Name can't be empty" }
+    if (Test-Path $Name) { throw "Package with that name already exists" }
+    if (Test-Path _template) { throw "Template package not found" }
+    cp _template $Name -Recurse
+
+    $nuspec = gc "$Name\template.nuspec"
+    rm "$Name\template.nuspec"
+
+    $nuspec = $nuspec -replace '<id>.+',               "<id>$Name</id>"
+    $nuspec = $nuspec -replace '<iconUrl>.+',          "<iconUrl>https://cdn.rawgit.com/majkinetor/chocolatey/master/$Name/icon.png</iconUrl>"
+    $nuspec = $nuspec -replace '<packageSourceUrl>.+', "<packageSourceUrl>https://github.com/majkinetor/chocolatey/tree/master/$Name</packageSourceUrl>"
+    $nuspec | Out-File -Encoding UTF8 "$Name\$Name.nuspec"
+}
+
+New-Package $Name
