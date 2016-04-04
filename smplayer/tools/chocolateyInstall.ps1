68bf9c50533b0c0285ddb01714b7fdb8ff8606b0
Miodrag Milic <miodrag.milic@gmail.com>
2016-04-04 15:03:27 +0200

    update

--- smplayer/tools/chocolateyInstall.ps1
+++ smplayer/tools/chocolateyInstall.ps1
@@ -1,14 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-
 $packageName  = 'smplayer'
 $url32        = 'http://www.fosshub.com/SMPlayer.html/smplayer-16.4.0-win32.exe'
 $url64        = 'http://www.fosshub.com/SMPlayer.html/smplayer-16.4.0-x64.exe'
-$chocoTempDir = Join-Path $Env:Temp "chocolatey"
-$toolsDir     = Split-Path $MyInvocation.MyCommand.Definition
-$genLinkFile  = "$chocoTempDir\$packageName\genlink.txt"
-$installerPath  = "$chocoTempDir\$packageName\smplayer-install.exe"
-
 
 function genLink($url) {
     $url = $url.Replace('http://www.fosshub.com', 'http://www.fosshub.com/genLink')
