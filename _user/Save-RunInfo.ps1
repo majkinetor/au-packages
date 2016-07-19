684363f2938916d72f32644621fe8c738dfbd1ac
Miodrag Milic <miodrag.milic@gmail.com>
2016-07-19 23:34:37 +0200

    bugfix

--- _user/Save-RunInfo.ps1
+++ _user/Save-RunInfo.ps1
@@ -1,4 +1,9 @@
 function Save-RunInfo {
     "Saving run info"
+    $p = $Info.Options.Mail.Password
+    $Info.Options.Mail.Password=''
+
     $Info | Export-CliXML update_info.xml
+
+    $Info.Options.Mail.Password = $p
 }
