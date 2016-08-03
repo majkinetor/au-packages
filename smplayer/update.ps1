9733dc4515fa1debc8546b8221eaeb654d32a67c
Miodrag Milic <miodrag.milic@gmail.com>
2016-08-03 21:13:17 +0200

    remove basic parsing on smplayer, it blocks.

--- smplayer/update.ps1
+++ smplayer/update.ps1
@@ -14,7 +14,7 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $download_page = Invoke-WebRequest -Uri $releases #-UseBasicParsing
 
     $re  = 'smplayer-.+\.exe$'
     $url = $download_page.links | ? href -match $re | select -expand href
