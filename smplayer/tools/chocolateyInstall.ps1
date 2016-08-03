4c1e7c6b0665742526697ebdf242f55e9525b14a
Miodrag Milic <miodrag.milic@gmail.com>
2016-08-03 20:58:49 +0200

    smplayer has checksums in package & uppdate.ps1 - #16

--- smplayer/tools/chocolateyInstall.ps1
+++ smplayer/tools/chocolateyInstall.ps1
@@ -3,13 +3,20 @@
 $packageName  = 'smplayer'
 $url32        = 'http://www.fosshub.com/SMPlayer.html/smplayer-16.7.0-win32.exe'
 $url64        = 'http://www.fosshub.com/SMPlayer.html/smplayer-16.7.0-x64.exe'
+$checksum32   = '54fd0c0b13d0350154e6aac4eaba4863d804788f733fc9a25364fb983edc6413'
+$checksum64   = 'ba76235158edd0788226e992ef495d59fb620e56b0833af34990091507c8b994'
 
 function genLink($url) {
     $url = $url.Replace('http://www.fosshub.com', 'http://www.fosshub.com/genLink')
     $url.Replace('SMPlayer.html', 'SMPlayer')
 }
 
-$url = if ((Get-ProcessorBits) -eq 64) { $url64 } else { $url32}
+if (Get-ProcessorBits 64) {
+    $url = $url64; $checksum = $checksum64
+} else {
+    $url = $url32; $checksum = $checksum32
+}
+
 $url = genLink $url
 $url = Get-UrlFromFosshub $url
 
@@ -18,6 +25,10 @@ $packageArgs = @{
   fileType               = 'EXE'
   url                    = $url
   url64bit               = $url
+  checksum               = $checksum
+  checksum64             = $checksum
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
   registryUninstallerKey = $packageName
--- smplayer/update.ps1
+++ smplayer/update.ps1
@@ -1,12 +1,14 @@
 import-module au
 
-$releases = 'http://smplayer.sourceforge.net/en/downloads'
+$releases = 'http://www.fosshub.com/SMPlayer.html'
 
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(^[$]url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
             "(^[$]url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -16,12 +18,16 @@ function global:au_GetLatest {
 
     $re  = 'smplayer-.+\.exe$'
     $url = $download_page.links | ? href -match $re | select -expand href
-    $url32 = $url[0]
-    $url64 = $url[1]
+    $url32 = 'http://www.fosshub.com' + $url[0]
+    $url64 = 'http://www.fosshub.com' + $url[1]
 
     $version  = $url64 -split '-' | select -Last 1 -Skip 1
 
-    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    $sha = $download_page.RawContent -split "`n" | sls 'SHA256'
+    $sha32 = $sha[0].ToString() -split ' |<' | select -Skip 1 -Last 1
+    $sha64 = $sha[1].ToString() -split ' |<' | select -Skip 1 -Last 1
+
+    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version; Checksum32 = $sha32; Checksum64 = $sha64 }
     return $Latest
 }
 
