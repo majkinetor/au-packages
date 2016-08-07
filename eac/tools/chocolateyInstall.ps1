c0780ea981950dc205d7a29a586167e5ce70fabc
Miodrag Milic <miodrag.milic@gmail.com>
2016-08-07 19:37:04 +0200

    each checksumed

--- eac/tools/chocolateyInstall.ps1
+++ eac/tools/chocolateyInstall.ps1
@@ -2,11 +2,14 @@
 
 $packageName = 'eac'
 $url32       = 'http://www.exactaudiocopy.de/eac-1.1.exe'
+$checksum32  = '25184ACB27EB113E57B2994424383DD141B77F7A1C3C07B779D3821FD6BE412D'
 
 $packageArgs = @{
   packageName            = $packageName
   fileType               = 'EXE'
   url                    = $url32
+  checksum               = $checksum32
+  checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
   registryUninstallerKey = "Exact Audio Copy"
--- eac/update.ps1
+++ eac/update.ps1
@@ -3,19 +3,22 @@ import-module au
 $releases = 'http://www.exactaudiocopy.de/en/index.php/weitere-seiten/download-from-alternative-servers-2'
 
 function global:au_SearchReplace {
-    @{".\tools\chocolateyInstall.ps1" = @{ "(^[$]url32\s*=\s*)('.*')" = "`$1'$($Latest.URL)'" }}
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
 }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re  = "eac.*\.exe"
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $re      = "eac.*\.exe"
+    $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $version = $url -split '-|/|.exe' | select -Last 1 -Skip 1
 
-    $version  = $url -split '-|/|.exe' | select -Last 1 -Skip 1
-
-    $Latest = @{ URL = $url; Version = $version }
-    return $Latest
+    return @{ URL32 = $url; Version = $version }
 }
 
-update
+update -ChecksumFor 32
