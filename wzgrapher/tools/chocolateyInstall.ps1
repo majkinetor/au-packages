af57d8dc36e6926b3d648a0649a8bb6423aea607
Miodrag Milic <miodrag.milic@gmail.com>
2016-08-07 22:01:00 +0200

    wzgrapher checksumed

--- wzgrapher/tools/chocolateyInstall.ps1
+++ wzgrapher/tools/chocolateyInstall.ps1
@@ -2,10 +2,13 @@
 
 $packageName = 'wzgapher'
 $url32       = 'http://www.walterzorn.de/en/app/wzgrapher_e.zip'
+$checksum32  = '0B48BF5CB477D7247CACBE500B1C52B9B9502FFF876275CD643560AB2FEDC60D'
 
 $packageArgs = @{
   packageName   = $packageName
   url           = $url32
+  checksum      = $checksum32
+  checksumType  = 'sha256'
   unzipLocation = Split-Path $MyInvocation.MyCommand.Definition
 }
 Install-ChocolateyZipPackage @packageArgs
