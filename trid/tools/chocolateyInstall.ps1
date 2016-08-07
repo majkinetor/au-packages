4be921731455272aad442cb2e3da6d68c3ec30c6
Miodrag Milic <miodrag.milic@gmail.com>
2016-08-07 21:51:23 +0200

    trid checksumed

--- trid/tools/chocolateyInstall.ps1
+++ trid/tools/chocolateyInstall.ps1
@@ -2,14 +2,19 @@
 
 $packageName = 'trid'
 $url_trid    = 'http://mark0.net/download/trid_w32.zip'
+$checksum    = 'EA7F82363912F5B3C79217BA8716425EC3F2514887F788DCD5A2821D0B1FC83F'
 $url_trid_db = 'http://mark0.net/download/triddefs.zip'
 
 $packageArgs = @{
   packageName   = $packageName
   url           = $url_trid
-  #unzipLocation = "$ENV:ChocolateyInstall\bin"
+  checksum      = $checksum
+  checksumType  = 'sha256'
   unzipLocation = Split-Path $MyInvocation.MyCommand.Definition
 }
 Install-ChocolateyZipPackage @packageArgs
 $packageArgs.url = $url_trid_db
+
+
+$packageArgs.checksum = ''
 Install-ChocolateyZipPackage @packageArgs
