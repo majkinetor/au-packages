ce787f0c490923df8601b2d2ff6c3fdec8ecf05f
Miodrag Milic <miodrag.milic@gmail.com>
2016-08-07 15:08:21 +0200

    dngrep updated with checksums

--- dngrep/tools/chocolateyInstall.ps1
+++ dngrep/tools/chocolateyInstall.ps1
@@ -2,8 +2,10 @@
 
 $packageName = 'dngrep'
 $exeName     = 'dnGREP.exe'
-$url64       = 'https://github.com/dnGrep/dnGrep/releases/download/v2.8.16.0/dnGREP.2.8.16.x64.msi'
 $url32       = 'https://github.com/dnGrep/dnGrep/releases/download/v2.8.16.0/dnGREP.2.8.16.x86.msi'
+$url64       = 'https://github.com/dnGrep/dnGrep/releases/download/v2.8.16.0/dnGREP.2.8.16.x64.msi'
+$checksum32  = 'CE4753735148E1F48FE0E1CD9AA4DFD019082F4F43C38C4FF4157F08D346700C'
+$checksum64  = '025BD4101826932E954AACD3FE6AEE9927A7198FEEFFB24F82FBE5D578502D18'
 
 $packageArgs = @{
   packageName            = $packageName
@@ -11,6 +13,10 @@ $packageArgs = @{
   url                    = $url32
   url64bit               = $url64
   silentArgs             = '/quiet'
+  checksum               = $checksum32
+  checksum64             = $checksum64
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
   validExitCodes         = @(0)
   registryUninstallerKey = $packageName
 }
--- dngrep/update.ps1
+++ dngrep/update.ps1
@@ -1,12 +1,14 @@
-import-module au
+remove-module au
 
 $releases = 'https://github.com/dnGrep/dnGrep/releases'
 
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
      }
 }
@@ -14,15 +16,13 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases #could use: 'https://api.github.com/repos/dnGrep/dnGrep/releases/latest' | select -expand Content | ConvertFrom-Json | select name, assets_url
 
-    $re = 'dnGREP.*.msi'
-    $url= $download_page.links | ? href -match $re | select -First 2 -expand href
-    $url64    = 'https://github.com' + $url[0]
-    $url32    = 'https://github.com' + $url[1]
+    $re      = 'dnGREP.*.msi'
+    $url     = $download_page.links | ? href -match $re | select -First 2 -expand href
+    $url64   = 'https://github.com' + $url[0]
+    $url32   = 'https://github.com' + $url[1]
+    $version = ($url[0] -split '\/' | select -Index 5).Substring(1)
 
-    $version  = ($url[0] -split '\/' | select -Index 5).Substring(1)
-
-    $Latest = @{ URL64 = $url64; URL32 = $url32; Version = $version }
-    return $Latest
+    return @{ URL64 = $url64; URL32 = $url32; Version = $version }
 }
 
 update
