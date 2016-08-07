4cc655104fa231a3533653ed68ebcbb535a24cc4
Miodrag Milic <miodrag.milic@gmail.com>
2016-08-07 19:42:05 +0200

    ampfontviewer checksumed

--- ampfontviewer/tools/chocolateyInstall.ps1
+++ ampfontviewer/tools/chocolateyInstall.ps1
@@ -2,10 +2,13 @@
 
 $packageName = 'ampfontviewer'
 $url32       = 'http://www.ampsoft.net/files/FontViewer.zip'
+$checksum32  = '1356F3976DCD54EEE73611D1617CA8D113EAE6C14FF0B8971A3D21444FD22554'
 
 $packageArgs = @{
   packageName   = $packageName
   url           = $url32
+  checksum      = $checksum32
+  checksumType  = 'sha256'
   unzipLocation = Split-Path $MyInvocation.MyCommand.Definition
 }
 Install-ChocolateyZipPackage @packageArgs
