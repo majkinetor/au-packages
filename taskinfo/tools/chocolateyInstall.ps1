5d42330b38f0980975da0b1210a469f506b883eb
Miodrag Milic <miodrag.milic@gmail.com>
2016-08-07 21:44:17 +0200

    taskinfo checksumed [skip ci]

--- taskinfo/tools/chocolateyInstall.ps1
+++ taskinfo/tools/chocolateyInstall.ps1
@@ -1,14 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'taskinfo'
-$url32       = 'http://www.iarsn.com/taskinfo/tskinf10_0.exe'
-$url64       = 'http://www.iarsn.com/taskinfo/tskinf10_0.exe'
+$url         = 'http://www.iarsn.com/taskinfo/tskinf10_0.exe'
+$checksum    = '0D2717ABB312F758B1F23FB4FA3E79535D6315E1D5CD3C0C804C10DAB12A1212'
 
 $packageArgs = @{
   packageName            = $packageName
   fileType               = 'EXE'
-  url                    = $url32
-  url64bit               = $url64
+  url                    = $url
+  url64bit               = $url
+  checksum               = $checksum
+  checksum64             = $checksum
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
   silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
   validExitCodes         = @(0)
   registryUninstallerKey = $packageName
@@ -20,11 +24,6 @@ if ($Env:ChocolateyPackageParameters -eq '/Register') {
     sp HKCU:\SOFTWARE\Iarsn\TaskInfo_10_0\Registration\Current PersonalUse 1
 }
 
-$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$key = Get-ItemProperty -Path @($machine_key6432, $machine_key, $local_key) -ErrorAction SilentlyContinue | ? { $_.DisplayName -like "$($packageArgs.registryUninstallerKey)*" }
-if ($key) {
-    $installLocation = $key.InstallLocation
-    if (Test-Path $installLocation)  { Write-Host "$packageName installed to '$installLocation'" }
-}
+$installLocation = Get-AppInstallLocation $packageArgs.registryUninstallerKey
+if ($installLocation)  { Write-Host "$packageName installed to '$installLocation'" }
+else { Write-Warning "Can't find $PackageName install location" }
