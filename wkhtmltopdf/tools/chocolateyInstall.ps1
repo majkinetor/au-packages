6b9224819905215ba26e3673b1913153227f6f14
Miodrag Milic <miodrag.milic@gmail.com>
2016-08-07 21:58:43 +0200

    wkhtmltopdf checksumed [skip ci]

--- wkhtmltopdf/tools/chocolateyInstall.ps1
+++ wkhtmltopdf/tools/chocolateyInstall.ps1
@@ -3,12 +3,18 @@
 $packageName = 'wkhtmltopdf'
 $url32       = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3.2/wkhtmltox-0.12.3.2_msvc2013-win32.exe'
 $url64       = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3.2/wkhtmltox-0.12.3.2_msvc2013-win64.exe'
+$checksum32  = 'F75F1C58470BE5FC71BABDF9E14C9DD9E801F7549354F7E1CF4AB1FA7BD5D962'
+$checksum64  = 'E7DF45C7FF3B703C5938B66EA3DC0BD951C8E4ED07285E16FBD609E1DB431FED'
 
 $packageArgs = @{
   packageName            = $packageName
   fileType               = 'EXE'
   url                    = $url32
   url64bit               = $url64
+  checksum               = $checksum32
+  checksum64             = $checksum64
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
   registryUninstallerKey = 'wkhtmltox'
--- wkhtmltopdf/update.ps1
+++ wkhtmltopdf/update.ps1
@@ -5,22 +5,23 @@ $releases = 'http://wkhtmltopdf.org/downloads.html'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(^[$]url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
-    $re    = 'wkhtmltox-.*\.exe'
 
-    $url   = $download_page.links | ? href -match $re | select -First 2 -expand href
-    $url32 = $url[0]; $url64 = $url[1]
-    $version  = $url32 -split  '[-_]' | select -Last 1 -Skip 2
+    $re      = 'wkhtmltox-.*\.exe'
+    $url     = $download_page.links | ? href -match $re | select -First 2 -expand href
+    $url32   = $url[0]; $url64 = $url[1]
+    $version = $url32 -split  '[-_]' | select -Last 1 -Skip 2
 
-    $Latest = @{ URL64 = $url64; URL32 = $url32; Version = $version }
-    return $Latest
+    return @{ URL64 = $url64; URL32 = $url32; Version = $version }
 }
 
 update
