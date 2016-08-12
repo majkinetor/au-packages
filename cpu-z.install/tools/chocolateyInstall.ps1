8a601969fdcd02bccd85c53878a0e128a90a9a0a
Miodrag Milic <miodrag.milic@gmail.com>
2016-08-12 21:33:08 +0200

    cpu-z install

---
+++ cpu-z.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'cpu-z.install'
+$url32       = 'http://download.cpuid.com/cpu-z/cpu-z_1.77-en.exe'
+$url64       = $url32
+$checksum32  = '6AE07BED483A14E0589A6776A162E729021815C95BDCBCB9A4E2E8F0BCBF3D94'
+$checksum64  = $checksum32
+
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'EXE'
+  url                    = $url32
+  url64bit               = $url64
+  checksum               = $checksum32
+  checksum64             = $checksum64
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+  validExitCodes         = @(0)
+  registryUninstallerKey = 'cpu-z'
+}
+Install-ChocolateyPackage @packageArgs
+
+$installLocation = Get-AppInstallLocation $packageArgs.registryUninstallerKey
+if ($installLocation)  {
+    Write-Host "$packageName installed to '$installLocation'"
+    Register-Application "$installLocation\cpuz.exe"
+    Write-Host "$packageName registered as cpuz"
+}
+else { Write-Warning "Can't find $PackageName install location" }
