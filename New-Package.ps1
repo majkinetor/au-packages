761a1597f1a963376337169c88f5995c49124ce1
Miodrag Milic <miodrag.milic@gmail.com>
2016-08-13 18:16:19 +0200

    better template and New-Package

--- New-Package.ps1
+++ New-Package.ps1
@@ -1,4 +1,4 @@
-param($Name)
+param($Name, $Type)
 
 <#
 .SYNOPSIS
@@ -7,7 +7,20 @@ param($Name)
 .DESCRIPTION
     This function creates a new package by copying the directory _template which contains desired package basic settings.
 #>
-function New-Package( $Name, $GithubUser) {
+function New-Package{
+    [CmdletBinding()]
+    param(
+        #Package name
+        [string] $Name,
+
+        #Type of the package
+        [ValidateSet('Portable', 'Installer')]
+        [string] $Type='Installer',
+
+        #Github repository in the form username/repository
+        [string] $GithubRepository
+    )
+
     if ($Name -eq $null) { throw "Name can't be empty" }
     if (Test-Path $Name) { throw "Package with that name already exists" }
     if (!(Test-Path _template)) { throw "Template for the packages not found" }
@@ -16,10 +29,29 @@ function New-Package( $Name, $GithubUser) {
     $nuspec = gc "$Name\template.nuspec"
     rm "$Name\template.nuspec"
 
+    Write-Verbose 'Fixing nuspec'
     $nuspec = $nuspec -replace '<id>.+',               "<id>$Name</id>"
-    $nuspec = $nuspec -replace '<iconUrl>.+',          "<iconUrl>https://cdn.rawgit.com/$GithubUser/chocolatey/master/$Name/icon.png</iconUrl>"
-    $nuspec = $nuspec -replace '<packageSourceUrl>.+', "<packageSourceUrl>https://github.com/$GithubUser/chocolatey/tree/master/$Name</packageSourceUrl>"
+    $nuspec = $nuspec -replace '<iconUrl>.+',          "<iconUrl>https://cdn.rawgit.com/$GithubRepository/master/$Name/icon.png</iconUrl>"
+    $nuspec = $nuspec -replace '<packageSourceUrl>.+', "<packageSourceUrl>https://github.com/$GithubRepository/tree/master/$Name</packageSourceUrl>"
     $nuspec | Out-File -Encoding UTF8 "$Name\$Name.nuspec"
+
+    switch ($Type)
+    {
+        'Installer' {
+            Write-Verbose 'Using installer template'
+            rm "$Name\tools\chocolateyInstallZip.ps1"
+            mv "$Name\tools\chocolateyInstallExe.ps1" "$Name\tools\chocolateyInstall.ps1"
+        }
+        'Portable' {
+            Write-Verbose 'Using portable template'
+            rm "$Name\tools\chocolateyInstallExe.ps1"
+            mv "$Name\tools\chocolateyInstallZip.ps1" "$Name\tools\chocolateyInstall.ps1"
+        }
+    }
+
+    Write-Verbose 'Fixing chocolateyInstall.ps1'
+    $installer = gc "$Name\tools\chocolateyInstall.ps1"
+    $installer -replace "(^[$]packageName\s*=\s*)('.*')", "`$1'$($Name)'" | sc "$Name\tools\chocolateyInstall.ps1"
 }
 
-New-Package -Name $Name -GithubUser majkinetor
+New-Package $Name $Type -GithubRepository majkinetor/chocolatey -Verbose
