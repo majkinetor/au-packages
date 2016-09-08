ad6cf77063f3265e0d8dd616c489965ba90cb92b
Miodrag Milic <miodrag.milic@gmail.com>
2016-09-08 20:36:58 +0200

    au moved to its own repository

--- au/tools/chocolateyUninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-
-Write-Host "Uninstalling module au"
-$module_dst = "$Env:ProgramFiles\WindowsPowerShell\Modules\$packageName\$Env:ChocolateyPackageVersion"
-rm -force -recurse $module_dst
