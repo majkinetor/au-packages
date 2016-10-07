e1c63e275ded476b7b023d966bccfa15177d6c91
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-07 12:15:17 +0200

    nirlauncher

--- nirlauncher/tools/chocolateyInstall.ps1
+++ nirlauncher/tools/chocolateyInstall.ps1
@@ -1,25 +1,37 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName  = 'nirlauncher'
-$url32        = 'http://download.nirsoft.net/nirsoft_package_1.19.105.zip'
-$checksum32   = '34b5764689af9c7ca7e53ac004d73e6279b10a3565c838b0f5a3e34d49000fca'
-$toolsPath    = Split-Path $MyInvocation.MyCommand.Definition
-$downloadPath = "$env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion\nirlauncher.zip"
+$packageName   = 'nirlauncher'
+$url32         = 'http://download.nirsoft.net/nirsoft_package_1.19.105.zip'
+$checksum32    = '34b5764689af9c7ca7e53ac004d73e6279b10a3565c838b0f5a3e34d49000fca'
+$download_path = "$env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion\nirlauncher.zip"
+$install_path  = "$(Get-ToolsLocation)\NirLauncher"
 
 $options =  @{ Headers=@{ Referer = 'http://launcher.nirsoft.net/download.html' } }
-Get-WebFile -Url $url32 -FileName $downloadPath -Options $options
+Get-WebFile -Url $url32 -FileName $download_path -Options $options
 
-#Write-Host "Adding NirLuancher utilities to the PATH if needed"
-#"$installDir","$installDir\Nirsoft" | % { Install-ChocolateyPath $_ "Machine" }
-
+Write-Host "Installing $packageName to '$install_path'"
 $packageArgs = @{
   packageName    = $packageName
-  url            = $downloadPath
-  url64Bit       = $downloadPath
+  url            = $download_path
+  url64Bit       = $download_path
   checksum       = $checksum32
   checksum64     = $checksum32
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
-  unzipLocation  = $toolsPath
+  unzipLocation  = $install_path
 }
 Install-ChocolateyZipPackage @packageArgs
+
+if (Get-ProcessorBits 64) {
+    Write-Host '64 bit architecture - overwriting x32 apps with x64 versions'
+    mv $install_path\Nirsoft\x64\* $install_path\Nirsoft -force
+    rm $install_path\Nirsoft\x64 -ea 0
+}
+Install-ChocolateyPath $install_path\Nirsoft "Machine"
+
+$launcher_path = "$install_path\$packageName.exe"
+Register-Application $launcher_path
+Write-Host "$packageName registered as $packageName"
+
+Install-BinFile $packageName $launcher_path
+
---
+++ nirlauncher/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName  = 'nirlauncher'
+$install_path = "$(Get-ToolsLocation)\NirLauncher"
+
+Write-Host "Removing $packageName from the '$install_path'"
+rm $install_path -Recurse -Force -ea 0
+
+$new_path = $Env:Path.Replace(";$install_path\Nirsoft", '')
+if ($new_path -eq $Env:PATH) { return }
+Write-Host "Removing $packageName from system PATH"
+[System.Environment]::SetEnvironmentVariable('PATH', $new_path, 'Machine')
+
+Write-Host "Removing $packageName shim"
+Uninstall-BinFile $packageName
