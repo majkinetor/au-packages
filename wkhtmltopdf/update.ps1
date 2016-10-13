3c8f1917cb586dd0c53df62b2453b464dbf49980
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-13 19:14:30 +0200

    (wkhtmltopdf) UBP added

--- wkhtmltopdf/update.ps1
+++ wkhtmltopdf/update.ps1
@@ -14,7 +14,7 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
 
     $re      = 'wkhtmltox-.*\.exe'
     $url     = $download_page.links | ? href -match $re | select -First 2 -expand href
