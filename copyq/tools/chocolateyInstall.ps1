c89ca00e8e9e20300309183f933cf8f9f69f9a4f
Chocolatey <chocolatey@realdimensions.net>
2016-10-23 13:55:39 +0000

    AU:  updated - copyq [skip ci]

--- copyq/tools/chocolateyInstall.ps1
+++ copyq/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 
 $packageName = 'copyq'
 $url32       = 'https://github.com/hluk/CopyQ/releases/download/v2.7.1/copyq-2.7.1-setup.exe'
-$checksum32  = '781787EF7DB6801A843DED62900380CCCF31A0E9917FA13A55B19932A8F35DAC'
+$checksum32  = '781787ef7db6801a843ded62900380cccf31a0e9917fa13a55b19932a8f35dac'
 $running     = if (ps $packageName -ea 0) { $true } else { $false }
 
 $packageArgs = @{
