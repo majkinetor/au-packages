27d76cf5842902b8a2be553126204f6307171bbb
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-13 11:22:53 +0100

    fixes

--- test_all.ps1
+++ test_all.ps1
@@ -30,7 +30,7 @@ $options = [ordered]@{
             Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
             NoAppVeyor  = $false                            #  Markdown: do not show AppVeyor build shield
             Title       = "Update Force Test - Group ${n}"
-            UserMessage = "[Update report](http://tiny.cc/v1u1ey) | **USING AU NEXT VERSION**"       #  Markdown, Text: Custom user message to show
+            UserMessage = "[Update report](https://gist.github.com/$Env:gist_id) | **USING AU NEXT VERSION**"       #  Markdown, Text: Custom user message to show
         }
     }
 
