7a8ad628a6813337f97510f308371663ab02ab9f
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-24 18:23:34 +0100

    (cpu-z) Guidelines

--- cpu-z.install/tools/chocolateyInstall.ps1
+++ cpu-z.install/tools/chocolateyInstall.ps1
@@ -1,32 +1,25 @@
 $ErrorActionPreference = 'Stop'
 
-$packageName = 'cpu-z.install'
-$url32       = 'http://download.cpuid.com/cpu-z/cpu-z_1.78-en.exe'
-$url64       = $url32
-$checksum32  = '2377cad64d919fff7d334db51e23f79d88251ddefe6290038aa27add0c3f3203'
-$checksum64  = $checksum32
-
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
-
 $packageArgs = @{
-  packageName            = $packageName
-  fileType               = 'EXE'
-  url                    = $url32
-  url64bit               = $url64
-  checksum               = $checksum32
-  checksum64             = $checksum64
-  checksumType           = 'sha256'
-  checksumType64         = 'sha256'
-  silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
-  validExitCodes         = @(0)
-  registryUninstallerKey = 'cpu-z'
+  packageName    = 'cpu-z.install'
+  fileType       = 'exe'
+  url            = 'http://download.cpuid.com/cpu-z/cpu-z_1.78-en.exe'
+  url64bit       = 'http://download.cpuid.com/cpu-z/cpu-z_1.78-en.exe'
+  checksum       = '1189805a0ec65d3daaa96f70eefac7fac7cb677dacb5a947e08c8fe9c5e08c6d'
+  checksum64     = '1189805a0ec65d3daaa96f70eefac7fac7cb677dacb5a947e08c8fe9c5e08c6d'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+  validExitCodes = @(0)
+  softwareName   = 'cpu-z'
 }
 Install-ChocolateyPackage @packageArgs
 
-$installLocation = Get-AppInstallLocation $packageArgs.registryUninstallerKey
+$packageName = $packageArgs.packageName
+$installLocation = Get-AppInstallLocation $packageArgs.softwareName
 if ($installLocation)  {
     Write-Host "$packageName installed to '$installLocation'"
     Register-Application "$installLocation\cpuz.exe"
     Write-Host "$packageName registered as cpuz"
 }
-else { Write-Warning "Can't find $PackageName install location" }
+else { Write-Warning "Can't find $packageName install location" }
--- cpu-z.install/update.ps1
+++ cpu-z.install/update.ps1
@@ -5,26 +5,31 @@ $releases = 'http://www.cpuid.com/softwares/cpu-z.htm'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
         }
     }
 }
 
 function global:au_GetLatest {
-    $re      = 'cpu-z.+exe'
+    $re = 'cpu-z.+exe'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url     = $download_page.links | ? href -match $re | select -First 1 -Expand href
+    $url = $download_page.links | ? href -match $re | select -First 1 -Expand href
 
     $download_page = Invoke-WebRequest -Uri $url -UseBasicParsing
-    $url     = $download_page.links | ? href -match $re | select -First 1 -Expand href
+    $url = $download_page.links | ? href -match $re | select -First 1 -Expand href
 
-    $version = $url -split '[_-]' | select -Last 1 -Skip 1
-
-    return @{ URL = $url; Version = $version }
+    @{
+        Version = $url -split '[_-]' | select -Last 1 -Skip 1
+        URL32   = $url
+    }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor 32 
+    update -ChecksumFor 32
 }
