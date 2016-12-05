e180bdf4af6b143942a5bbeed89ea00175e22ef5
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-05 12:23:06 +0100

    (less) binaries removed; verification.txt added [not tested]

--- less/update.ps1
+++ less/update.ps1
@@ -3,35 +3,39 @@ import-module au
 $releases = 'http://guysalias.tk/misc/less/'
 
 function global:au_SearchReplace {
-    @{}
+    @{
+        ".\tools\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"        = "`${1} $($Latest.URL32)"
+          "(?i)(checksum32:).*"    = "`${1} $($Latest.Checksum32)"
+        }
+    }
 }
 
 function global:au_BeforeUpdate {
-    if (!(gc 7za -ea 0)) { choco install 7zip.commandline }
+    set-alias 7z $Env:chocoInstall\bin\7z.exe
 
     $lessdir = "$PSScriptRoot\less-*-win*"
     rm $lessdir -Recurse -Force -ea ignore
 
     iwr $Latest.URL -OutFile "$PSScriptRoot\less.7z"
-    7za x $PSScriptRoot\less.7z
+    7z x $PSScriptRoot\less.7z
 
-    rm $PSScriptRoot\tools\* -Recurse -Force
+    rm $PSScriptRoot\tools\* -Recurse -Force -Exclude VERIFICATION.txt
     mv $lessdir\* $PSScriptRoot\tools -Force
     rm $lessdir -Recurse -Force -ea ignore
     rm $PSScriptRoot\less.7z -ea ignore
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re    = 'less-.+win.+\.7z'
-    $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
-
+    $re  = 'less-.+win.+\.7z'
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
     $version = "$( ($url -split '-' | select -Index 1) / 100 )"
-    $url = $releases + $url
-
-    $Latest = @{ URL = $url; Version = $version }
-    return $Latest
+    @{
+       URL32   = $releases + $url
+       Version = $version
+    }
 }
 
 try {
