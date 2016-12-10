4471f6fed1666cac713faa26e45f7d489241c4db
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-10 19:35:49 +0100

    ...

--- _template/tools/chocolateyInstallEmbededExe.ps1
+++ _template/tools/chocolateyInstallEmbededExe.ps1
@@ -1,18 +1,22 @@
 $ErrorActionPreference = 'Stop'
 
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+$installerFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Installing x64 bit version"; gi "$toolsDir\*_x64.exe"
+} else { Write-Host "Installing x32 bit version"; gi "$toolsDir\*_x32.exe" }
+
 
 $silentArgs = @('/S')
 
 $packageArgs = @{
   packageName    = ''
   fileType       = 'exe'
-  file           = gi "$toolsDir\*.exe"
+  file           = gi "$toolsDir\*.exe" # $installerFile
   silentArgs     = $silentArgs
   validExitCodes = @(0, 1223)
 }
 Install-ChocolateyInstallPackage @packageArgs
-rm $toolsDir\*.exe
+rm ($toolsDir + '\*.' + $packageArgs.fileType)
 
 $packageName = $packageArgs.packageName
 $installLocation = Get-AppInstallLocation $packageName
