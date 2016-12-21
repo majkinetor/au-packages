2336e45fe394a3b08aa7ee3d0087c93ecf8e23e2
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-21 13:11:38 +0100

    using devel AU branch

--- update_vars_default.ps1
+++ update_vars_default.ps1
@@ -9,6 +9,6 @@ $Env:mail_enablessl   = 'true'
 
 $Env:api_key          = ''          #Chocolatey api key
 $Env:gist_id          = ''          #Specify your gist id or leave empty for anonymous gist
-$Env:github_user_repo = ''          #{github_user>/{repo}
+$Env:github_user_repo = ''          #{github_user}/{repo}
 $Env:github_api_key   = ''          #Github personal access token
 $Env:au_Push          = 'false'     #Push to chocolatey
