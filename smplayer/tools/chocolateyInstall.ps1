7d72715ff0d49b6d627d5598ac5dceb213e90e06
Miodrag Milić <miodrag.milic@gmail.com>
2017-01-23 15:24:07 +0100

    Update chocolateyInstall.ps1

--- smplayer/tools/chocolateyInstall.ps1
+++ smplayer/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir      = Split-Path $MyInvocation.MyCommand.Definition
-$installerFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') { gi "$toolsDir\*x64.exe" } else { gi "$toolsDir\*win32.exe" }
+$installerFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') { gi "$toolsDir\*x64.exe" } else { gi "$toolsDir\*x32.exe" }
 
 $packageArgs = @{
   packageName    = 'smplayer'
