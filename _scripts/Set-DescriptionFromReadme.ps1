dfe5a8452567dfaa774e43140a21cf352d319fb8
Miodrag Milic <miodrag.milic@gmail.com>
2017-02-20 19:44:11 +0100

    docs

--- _scripts/Set-DescriptionFromReadme.ps1
+++ _scripts/Set-DescriptionFromReadme.ps1
@@ -1,3 +1,22 @@
+<#
+.SYNOPSIS
+  Updates nuspec file description from README.md
+
+.DESCRIPTION
+  This script should be called in au_AfterUpdate to put the text in the README.md
+  into description tag of the Nuspec file. The current description will be replaced.
+  Function will throw an error if README.md is not found.
+
+.PARAMETER SkipFirst
+  Number of start lines to skip from the README.md, by default 0.
+
+
+.PARAMETER SkipLast
+  Number of end lines to skip from the README.md, by default 0.
+
+.EXAMPLE
+  function global:au_AfterUpdate  { Set-DescriptionFromReadme -SkipFirst 2 }
+#>
 function Set-DescriptionFromReadme([int]$SkipFirst=0, [int]$SkipLast=0) {
     if (!(Test-Path README.md)) { throw 'Set-DescriptionFromReadme: README.md not found' }
 
@@ -11,8 +30,6 @@ function Set-DescriptionFromReadme([int]$SkipFirst=0, [int]$SkipLast=0) {
     $nu = $nu -replace "(?smi)(\<description\>).*?(\</description\>)", "`${1}$($description)`$2"
 
     $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
-    $n = (Resolve-Path $NuspecFileName)
-    [System.IO.File]::WriteAllText($n, $nu, $Utf8NoBomEncoding)
+    $NuPath = (Resolve-Path $NuspecFileName)
+    [System.IO.File]::WriteAllText($NuPath, $nu, $Utf8NoBomEncoding)
 }
-
-sal readme2desc Set-DescriptionFromReadme
