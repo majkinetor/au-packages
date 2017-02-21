7714482085465d56e2333f56a8b9759bac93a328
Miodrag Milic <miodrag.milic@gmail.com>
2017-02-21 21:32:06 +0100

    (shutup10) new package

---
+++ shutup10/README.md
@@ -0,0 +1,9 @@
+# ![icon](https://cdn.rawgit.com/majkinetor/chocolatey/master/shutup10/icon.png) [ShutUp10](https://chocolatey.org/packages/shutup10)
+
+O&amp;O ShutUp10 means you have full control over which comfort functions under Windows 10 you wish to use, and you decide when the passing on of your data goes too far.
+
+Using a very simple interface, you decide how Windows 10 should respect your privacy by deciding which unwanted functions should be deactivated.
+O&amp;O ShutUp10 is entirely free and does not have to be installed – it can be simply run directly and immediately on your PC. And it will not install or download retrospectively unwanted or unnecessary software, like so many other programs do these days!
+
+![screenshot](https://cdn.rawgit.com/majkinetor/chocolatey/master/shutup10/screenshot.png)
+
---
+++ shutup10/update.ps1
@@ -0,0 +1,38 @@
+import-module au
+. $PSScriptRoot\..\_scripts\all.ps1
+
+$releases    = 'https://www.oo-software.com/en/shutup10'
+
+function global:au_SearchReplace {
+   @{
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+          "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL32)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge
+    mv $PSScriptRoot\tools\*.exe $PSScriptRoot\tools\OOSU10.exe
+}
+
+function global:au_AfterUpdate  { Set-DescriptionFromReadme -SkipFirst 2 }
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $re = '\.exe$'
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+    Write-Host $url
+
+    $version = $download_page.AllElements | ? {$_.TagName -eq 'h4' -and $_.InnerText -like 'Version *'} | select -Expand InnerText -First 1 | % { $_ -replace 'Version ' }
+
+    @{
+        URL32        = $url
+        Version      = $version.Replace('v','')
+    }
+}
+
+update -ChecksumFor none
