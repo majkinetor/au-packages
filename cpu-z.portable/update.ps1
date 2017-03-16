9f6ae2b7b33c2624e3cfe8934bea3657d9dca007
Miodrag Milic <miodrag.milic@gmail.com>
2017-03-16 09:40:33 +0100

    (cpu-z.portable) fixed updater

--- cpu-z.portable/update.ps1
+++ cpu-z.portable/update.ps1
@@ -6,7 +6,7 @@ $releases = 'http://www.cpuid.com/softwares/cpu-z.htm'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
@@ -32,11 +32,12 @@ function global:au_GetLatest {
     }
 
     @{
-        Version = $url -split '[_-]' | select -Last 1 -Skip 1
-        URL32   = $url
+        Version    = $url -split '[_-]' | select -Last 1 -Skip 1
+        URL32      = $url
+        Checksum32 = $remote_checksum
     }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
 if ($global:au_old_force -is [bool]) { $global:au_force = $global:au_old_force }
 
