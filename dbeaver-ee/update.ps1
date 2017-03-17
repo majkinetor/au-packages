0b11383687577df67013eb447d5a3f712cecf08f
Miodrag Milic <miodrag.milic@gmail.com>
2017-03-17 18:50:19 +0100

    (dbeaver-ee) fixed updater

--- dbeaver-ee/update.ps1
+++ dbeaver-ee/update.ps1
@@ -1,47 +1,30 @@
 import-module au
 . $PSScriptRoot\..\_scripts\all.ps1
 
-$releases = 'https://github.com/serge-rider/dbeaver/releases'
+$releases = 'http://dbeaver.jkiss.org/files/dbeaver-ee-latest-x86-setup.exe'
 
 function global:au_SearchReplace {
-   @{
+    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-            "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
-            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
-        }
-
-        "$($Latest.PackageName).nuspec" = @{
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
-        }
-
-        ".\legal\VERIFICATION.txt" = @{
-          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
-          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
-          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
-          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
-          "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL64)"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 function global:au_AfterUpdate  { Set-DescriptionFromReadme -SkipFirst 2 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $request = [System.Net.WebRequest]::Create($releases)
+    $request.AllowAutoRedirect=$false
+    $response=$request.GetResponse()
+    $url32 = $response.GetResponseHeader('Location')
+    $url64 = $url32 -replace '-x86-', '-x86_64-'
 
-    $re    = '\.exe$'
-    $url   = $download_page.links | ? href -match $re | select -Expand href -First 2 | % { 'https://github.com' + $_ }
-
-    $version  = $url[0] -split '/' | select -Last 1 -Skip 1
-
-    @{
-        Version      = $version
-        URL32        = $url | ? {$_ -notmatch 'x86_64'} | select -First 1
-        URL64        = $url | ? {$_ -match    'x86_64'} | select -First 1
-        ReleaseNotes = "https://github.com/serge-rider/dbeaver/releases/tag/${version}"
-    }
+    $version = $url32 -split '-' | select -Last 1 -Skip 2
+    @{ URL64 = $url64; URL32 = $url32; Version = $version }
 }
 
-update -ChecksumFor none
+update -NoCheckUrl
