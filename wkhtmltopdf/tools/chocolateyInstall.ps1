c86e3ba9b6e2e253eb9e058f837b9d384e28910c
Miodrag Milic <miodrag.milic@gmail.com>
2017-03-20 08:08:28 +0100

    (wkhtmltopdf) fixed softwareName [AU wkhtmltopdf]

--- wkhtmltopdf/tools/chocolateyInstall.ps1
+++ wkhtmltopdf/tools/chocolateyInstall.ps1
@@ -7,21 +7,21 @@ $checksum32  = '6883d1456201bc9d421cb7dd32a99458be3d56631ea4f292e51b3c1aecbe2723
 $checksum64  = '14a5996adc77dc606944dbc0dc682bff104cd38cc1bec19253444cb87f259797'
 
 $packageArgs = @{
-  packageName            = $packageName
-  fileType               = 'EXE'
-  url                    = $url32
-  url64bit               = $url64
-  checksum               = $checksum32
-  checksum64             = $checksum64
-  checksumType           = 'sha256'
-  checksumType64         = 'sha256'
-  silentArgs             = '/S'
-  validExitCodes         = @(0)
-  registryUninstallerKey = 'wkhtmltox'
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url32
+  url64bit       = $url64
+  checksum       = $checksum32
+  checksum64     = $checksum64
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'wkhtmltox*'
 }
 Install-ChocolateyPackage @packageArgs
 
-$installLocation = Get-AppInstallLocation $packageArgs.registryUninstallerKey
+$installLocation = Get-AppInstallLocation $packageArgs.softwareName
 if ($installLocation)  { Write-Host "$packageName installed to '$installLocation'" }
 else { Write-Warning "Can't find $PackageName install location" }
 
