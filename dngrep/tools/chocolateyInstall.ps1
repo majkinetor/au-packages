f900df31303aa1da0eee67f855f944245588d01c
Miodrag Milic <miodrag.milic@gmail.com>
2017-03-25 17:20:57 +0100

    (dngrep) Uninstaller
    closes #32

--- dngrep/tools/chocolateyInstall.ps1
+++ dngrep/tools/chocolateyInstall.ps1
@@ -8,25 +8,23 @@ $checksum32  = 'CE4753735148E1F48FE0E1CD9AA4DFD019082F4F43C38C4FF4157F08D346700C
 $checksum64  = '025BD4101826932E954AACD3FE6AEE9927A7198FEEFFB24F82FBE5D578502D18'
 
 $packageArgs = @{
-  packageName            = $packageName
-  fileType               = 'MSI'
-  url                    = $url32
-  url64bit               = $url64
-  silentArgs             = '/quiet'
-  checksum               = $checksum32
-  checksum64             = $checksum64
-  checksumType           = 'sha256'
-  checksumType64         = 'sha256'
-  validExitCodes         = @(0)
-  registryUninstallerKey = $packageName
+  packageName    = $packageName
+  fileType       = 'MSI'
+  url            = $url32
+  url64bit       = $url64
+  silentArgs     = '/quiet'
+  checksum       = $checksum32
+  checksum64     = $checksum64
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0)
+  softwareName   = "dngrep*"
 }
 Install-ChocolateyPackage @packageArgs
 
-$exePath = gp HKLM:\SOFTWARE\Classes\Directory\background\shell\dnGREP\command -ea 0 | select -expand '(default)'
-$exePath = $exePath -split '"' | select -Index 1
-if (Test-Path $exePath) {
-    "Installation directory: " + (Split-Path $exePath -Parent)
-    $AppPathKey = "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\$exeName"
-    If (!(Test-Path $AppPathKey)) {New-Item "$AppPathKey" | Out-Null}
-    Set-ItemProperty -Path $AppPathKey -Name "(Default)" -Value $exePath
-}
+$installLocation = Get-AppInstallLocation $packageName
+if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+Write-Host "$packageName installed to '$installLocation'"
+
+Register-Application "$installLocation\$packageName.exe"
+Write-Host "$packageName registered as $packageName"
