aa4385310cf153db80172bd89e58eb6487e919ae
Miodrag Milic <miodrag.milic@gmail.com>
2017-03-25 17:00:40 +0100

    (eraser) refactoring to remove 7zip dependency
    closes #28

--- eraser/tools/chocolateyInstall.ps1
+++ eraser/tools/chocolateyInstall.ps1
@@ -1,37 +1,28 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'eraser'
-$url32       = 'https://sourceforge.net/projects/eraser/files/Eraser%206/6.2/Eraser%206.2.0.2979.exe'
-$url64       = $url32
+$url         = 'https://sourceforge.net/projects/eraser/files/Eraser%206/6.2/Eraser%206.2.0.2979.exe'
 $checksum    = '090968D0D9C386924A2838C484855595890DA9879544C7D8B4B201F9756712C6'
 
 $setupDir    = Get-PackageCacheLocation
 
-$setupPath  = "$setupDir\eraser.exe"
 $packageArgs = @{
-  packageName  = $packageName
-  fileFullPath = $setupPath
-  url          = $url32
-  url64bit     = $url64
-  checksum     = $checksum
-  checksumType = 'sha256'
+  packageName    = $packageName
+  url            = $url
+  url64Bit       = $url
+  checksum       = $checksum
+  checksum64     = $checksum
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = $setupDir
 }
-Get-ChocolateyWebFile @packageArgs
-
-if (!(gcm 7z -ea 0)) {
-    $path = gp HKCU:\Software\7-Zip Path | select -Expand Path
-    if (!(Test-Path "$path\7z.exe")) { throw "Can not find 7z.exe" }
-    sal 7z "$path\7z.exe"
-}
-
-cd $setupDir
-7z x -aoa $setupPath -o*
+Install-ChocolateyZipPackage @packageArgs
 
 $msi_path = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
-         Write-Host "Installing 64 bit version"; ls "$setupDir\*eraser*x86*.msi" -Recurse | Select -Expand FullName
-} else { Write-Host "Installing 32 bit version"; ls "$setupDir\*eraser*x64*.msi" -Recurse | Select -Expand FullName }
-
-
+    ls "$setupDir\*eraser*x86*.msi" -Recurse | Select -Expand FullName
+} else {
+    ls "$setupDir\*eraser*x64*.msi" -Recurse | Select -Expand FullName
+}
 if (!(Test-Path $msi_path)) { throw "Unpacking failed" }
 
 $packageArgs = @{
