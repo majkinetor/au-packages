ae49fe7c88ff95c26211087f870d1c6e60ec638e
Chocolatey <chocolatey@realdimensions.net>
2017-04-03 00:10:27 +0000

    AU: 2 updated - dbeaver dbeaver-ee [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/5ea278b5cd87f77abba527c6ccba287dc9e4476f

--- dbeaver-ee/tools/chocolateyInstall.ps1
+++ dbeaver-ee/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'dbeaver'
-$url32       = 'http://dbeaver.jkiss.org/files/3.8.5/dbeaver-ee-3.8.5-x86-setup.exe'
-$url64       = 'http://dbeaver.jkiss.org/files/3.8.5/dbeaver-ee-3.8.5-x86_64-setup.exe'
-$checksum32  = 'fa9d27d3e9e212c68e46bfe1693e75a0a755919fb9ed7907360ebc190ab69e11'
-$checksum64  = 'ff552ddd92dfd870248905687769297748014df4f0e317a7a9dfcb49ff0c2a37'
+$url32       = 'http://dbeaver.jkiss.org/files/4.0.4/dbeaver-ee-4.0.4-x86-setup.exe'
+$url64       = 'http://dbeaver.jkiss.org/files/4.0.4/dbeaver-ee-4.0.4-x86_64-setup.exe'
+$checksum32  = '5f900e1a142d7960abeaa5063e031adf7d9e78ca665bd16236bc7bf1d053c88b'
+$checksum64  = 'afc159464306705851d4af2295b5f0d7cac1e0844400e8e9d732d9e466f1bd7e'
 
 $packageArgs = @{
   packageName            = $packageName
