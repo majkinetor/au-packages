a7e053b93258ddea6f1f80465ae0ae40955d2b1e
Miodrag Milić <miodrag.milic@gmail.com>
2017-04-21 09:35:45 +0200

    Update register_example.ps1

--- gitlab-runner/tools/register_example.ps1
+++ gitlab-runner/tools/register_example.ps1
@@ -1,13 +1,15 @@
 # For the list of all environment vars run `gitlab-runner register --help`
 
-$Env:CONFIG_FILE                = "$PSScriptRoot\config.toml"
 $Env:REGISTRATION_TOKEN         = ''
 $Env:CI_SERVER_URL              = ''
-$Env:RUNNER_NAME                = ''
 $Env:RUNNER_TAG_LIST            = ''
-$Env:REGISTER_RUN_UNTAGGED      = 'false'
+
+$Env:RUNNER_NAME                = ''
 $Env:RUNNER_EXECUTOR            = 'shell'
 $Env:RUNNER_SHELL               = 'powershell'
+
+$Env:CONFIG_FILE                = "$PSScriptRoot\config.toml"
+$Env:REGISTER_RUN_UNTAGGED      = 'false'
 $Env:RUNNER_REQUEST_CONCURRENCY = 1
 $Env:RUNNER_BUILDS_DIR          = ''
 $Env:RUNNER_CACHE_DIR           = ''
