ccf3cb2531e5d8408daf4333d69aa6acbf167be3
Miodrag Milic <miodrag.milic@gmail.com>
2017-04-26 23:43:04 +0200

    func

--- cinst-majkinetor.ps1
+++ cinst-majkinetor.ps1
@@ -1,11 +1,12 @@
 # Installs package directly from repository
 # Example: cinst-majkinetor furmark
+function cinst-majkinetor {
+    param($Name, $Repo = "https://github.com/majkinetor/au-packages/tree/master" )
 
-param($Name, $Repo = "https://github.com/majkinetor/au-packages/tree/master" )
-
-$download_page = iwr $Repo/$Name -UseBasicParsing
-$url = $download_page.Links.href -like '*.nupkg'
-$p = $url -split '/' | select -last 1
-iwr "https://rawgit.com/majkinetor/au-packages/master/$(($p -split '\.')[0])/$p" -Out $p
-cinst $p
-rm $p
+    $download_page = iwr $Repo/$Name -UseBasicParsing
+    $url = $download_page.Links.href -like '*.nupkg'
+    $p = $url -split '/' | select -last 1
+    iwr "https://rawgit.com/majkinetor/au-packages/master/$(($p -split '\.')[0])/$p" -Out $p
+    cinst $p
+    rm $p
+}
