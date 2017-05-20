2a005130d4579d52e05c7b071005b39c5587c915
Miodrag Milic <miodrag.milic@gmail.com>
2017-05-20 18:04:59 +0200

    template fixes for uninstaller

--- _template/tools/chocolateyUninstall.ps1
+++ _template/tools/chocolateyUninstall.ps1
@@ -3,7 +3,6 @@
 $packageName         = ''
 $softwareNamePattern = ''
 
-
 [array] $key = Get-UninstallRegistryKey $softwareNamePattern
 if ($key.Count -eq 1) {
     $key | % {
@@ -12,9 +11,8 @@ if ($key.Count -eq 1) {
             silentArgs             = "/x86=0 /S"
             fileType               = 'EXE'
             validExitCodes         = @(0)
-            file                   = ''
+            file                   = "$($_.UninstallString.Replace(' /x86=0', ''))"   #"C:\Program Files\OpenSSH\uninstall.exe" /x86=0
         }
-        $packageArgs.file = "$($_.UninstallString.Replace(' /x86=0', ''))"   #"C:\Program Files\OpenSSH\uninstall.exe" /x86=0
         Uninstall-ChocolateyPackage @packageArgs
     }
 }
@@ -22,9 +20,9 @@ elseif ($key.Count -eq 0) {
     Write-Warning "$packageName has already been uninstalled by other means."
 }
 elseif ($key.Count -gt 1) {
-    Write-Warning "$key.Count matches found!"
+    Write-Warning "${$key.Count} matches found!"
     Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
     Write-Warning "Please alert package maintainer the following keys were matched:"
-    $key | % {Write-Warning "- $_.DisplayName"}
+    $key | % {Write-Warning "- ${$_.DisplayName}"}
 }
 
