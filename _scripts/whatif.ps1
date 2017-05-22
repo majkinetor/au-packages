2772781d97fe5ecb8c6523b626a16e2ed9bb61fd
Miodrag Milic <miodrag.milic@gmail.com>
2017-05-22 21:23:49 +0200

    whatif docs

--- _scripts/whatif.ps1
+++ _scripts/whatif.ps1
@@ -1,3 +1,21 @@
+<#
+This script implements WhatIf functionality. When you use it, it will 
+    - backup a package prior a call to update
+    - save updater output to another folder
+    - restore an original package after update finishes
+
+Use it by adding a switch parameter to update.ps1 and surrounding update
+by backup and restore functions:
+
+    param( [switch]$WhatIf )
+
+    ...
+    
+    backup 
+    update ...
+    restore
+
+#>
 
 function backup()  { 
     if (!$WhatIf) { return }
@@ -13,7 +31,7 @@ function backup()  {
 function restore() { 
     $packageName = Split-Path -Leaf $MyInvocation.PSScriptRoot
     $d = "$Env:TEMP\au\$packageName"
-    
+
     cp . $d\_output -Recurse 
     rm .\* -Recurse
     cp $d\_backup\* . -Recurse 
