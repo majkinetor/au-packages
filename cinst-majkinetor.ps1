ce1c58a0a3500cdc041560518dd2fa309ffb9630
Miodrag Milić <miodrag.milic@gmail.com>
2017-06-01 00:39:03 +0200

    Update cinst-majkinetor.ps1

--- cinst-majkinetor.ps1
+++ cinst-majkinetor.ps1
@@ -1,20 +1,27 @@
 <# Installs package directly from Github repository
    To use with your own repo: 
-     - Rename the function to contain your Github name
-     - Set the path to your packages root in $Repo 
-     - Create short link, via for example goo.gl   
-   Example: 
-     iwr https://goo.gl/0aP4PK | iex; cinst-majkinetor furmark
-#>
-function cinst-majkinetor {
-    param($Name, $Repo = "https://github.com/majkinetor/au-packages/tree/master" )
+     - Set the path to your packages root in $Repo
+     - Create short link to raw github file via for example goo.gl 
 
-    $download_page = iwr $Repo/$Name -UseBasicParsing
+   Usage:
+     - Pass repository package name as a first argument
+     - Pass any cinst option after that (some may not work ofc. such as `version`)
+
+   Example: 
+     iwr https://goo.gl/0aP4PK | iex; cinst-gh furmark --force
+#>
+function cinst-gh {
+    $Repo = "https://github.com/majkinetor/au-packages/tree/master" 
+
+    $name = $args[0]
+    $download_page = iwr $Repo/$name -UseBasicParsing
     $url = $download_page.Links.href -like '*.nupkg'
     $p = $url -split '/' | select -last 1
     
     $raw = $Repo -replace 'github.com', 'rawgit.com' -replace 'tree/'
     iwr "$raw/$(($p -split '\.')[0])/$p" -Out $p
-    cinst $p
+    $a = $args | select -Skip 1
+    $cmd = "cinst $p $a"
+    Write-Host $cmd; iex $cmd
     rm $p
 }
