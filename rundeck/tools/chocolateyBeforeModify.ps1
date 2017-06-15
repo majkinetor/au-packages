1c5d4d32f03c2fcca0566ac715a5d02a2d3087e2
Miodrag Milic <miodrag.milic@gmail.com>
2017-06-15 22:50:47 +0200

    (rundeck) bugfix [AU rundeck:2.8.2]

--- rundeck/tools/chocolateyBeforeModify.ps1
+++ rundeck/tools/chocolateyBeforeModify.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 
-if (!($Env:RDECK_BASE -and (Test-Path $Env:RDECK_BASE)) { throw "RDECK_BASE environment variable is invalid" }
+if (!($Env:RDECK_BASE -and (Test-Path $Env:RDECK_BASE))) { throw "RDECK_BASE environment variable is invalid" }
 
 if (Get-Service rundeck -ea 0) { Stop-Service rundeck }
 
