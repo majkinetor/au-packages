5ece600805c91c4e4b4cc7160ae8642c014ed92f
Miodrag Milic <miodrag.milic@gmail.com>
2017-06-20 13:34:03 +0200

    (copyq) added new parameter /ToggleShortcut

--- copyq/tools/chocolateyInstall.ps1
+++ copyq/tools/chocolateyInstall.ps1
@@ -30,4 +30,21 @@ Write-Host "$packageName installed to '$installLocation'"
 Register-Application "$installLocation\$packageName.exe"
 Write-Host "$packageName registered as $packageName"
 
+if ($pp.ToggleShortcut) {  
+  Write-Host "Using global toggle shortcut:" $pp.ToggleShortcut
+  $config_path = "$Env:APPDATA\copyq\copyq.ini"
+  mkdir (Split-Path $config_path) -ea 0 | Out-Null
+  if (Test-Path $config_path) {  $ini = gc $config_path -Encoding UTF8 | Out-String } 
+  if ($ini -notmatch 'copyq: toggle\(\)') {
+    $cmnd = "
+[Command]
+Command=copyq: toggle()
+GlobalShortcut=$($pp.ToggleShortcut)
+Icon=\xf022
+Name=Show/hide main window
+"
+  ($ini -replace '\[Commands\]\s*size=0') + $cmnd  | Out-File -Encoding UTF8 -Append $config_path
+  }
+}
+
 start "$installLocation\$packageName.exe"
