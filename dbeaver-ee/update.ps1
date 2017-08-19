6376490f9b96a1e33875f2f67f8ce26bacbe4339
Miodrag Milic <miodrag.milic@gmail.com>
2017-08-19 10:25:58 +0200

    (dbeaver-ee) fixed updater (releases URL changed)

--- dbeaver-ee/update.ps1
+++ dbeaver-ee/update.ps1
@@ -1,7 +1,7 @@
 import-module au
 . $PSScriptRoot\..\_scripts\all.ps1
 
-$releases = 'http://dbeaver.jkiss.org/files/dbeaver-ee-latest-x86-setup.exe'
+$releases = 'https://dbeaver.com/files/dbeaver-ee-latest-x86-setup.exe'
 
 function global:au_SearchReplace {
     @{
