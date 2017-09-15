3d56fb4a565a329f6381b7f6ae4733643f4293c9
Miodrag Milic <miodrag.milic@gmail.com>
2017-09-15 14:58:06 +0200

    (pass-winmenu) docs and fixes

--- pass-winmenu/README.md
+++ pass-winmenu/README.md
@@ -9,4 +9,15 @@ Pass-winmenu follows the philosophy of (and is compatible with) the Linux passwo
 - Because the passwords are simply stored in a directory tree, it's easy to synchronise your password store using any version control software of your choosing, giving you synchronisation, file history, and redundancy
 - The password files are always encrypted and can only be decrypted with your private GPG key, which is secured with a passphrase.
 
+## Package Parameters
+
+- `/InstallDir` - Installation location, by default `$(Get-ToolsLocation)\pass-winmenu`.
+- `/PasswordStore` - Location of the password store, by default `$HOME\.password-store`.
+- `/GpgId` - Email address of the GPG key to be used.
+
+## Notes
+
+- You need to have an existing GPG key. If not, the store will not work until you create one using `gpg --gen-key`.
+- If you use git to sync the passwords with multiple environments, you need to install it along with this package since git synchronization is not mandatory: `cinst pass-winmenu git`.
+
 ![screenshot](https://cdn.rawgit.com/majkinetor/chocolatey/master/pass-winmenu/screenshot.gif)
--- pass-winmenu/tools/chocolateyInstall.ps1
+++ pass-winmenu/tools/chocolateyInstall.ps1
@@ -1,10 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$running = ps pass-winmenu -ea 0 | kill -PassThru
 
 $pp = Get-PackageParameters
-$installDir = if ($pp.InstallDir ) { $pp.InstallDir } else { Join-Path (Get-ToolsLocation) $Env:ChocolateyPackageName }
-Write-Host "Sysinternals Suite is going to be installed in '$installDir'"
+$installDir    = if ($pp.InstallDir ) { $pp.InstallDir } else { "$(Get-ToolsLocation)\pass-winmenu" }
+$passwordStore = if ($pp.PasswordStore) { $pp.PasswordStore } else {  "$Home\.password-store" }
 
 $packageArgs = @{
     PackageName    = 'pass-winmenu'
@@ -14,3 +15,21 @@ $packageArgs = @{
 
 Get-ChocolateyUnzip @packageArgs
 rm $toolsPath\*.zip -ea 0
+cp $installDir\pass-winmenu\* $installDir -Recurse -Force
+rm $installDir\pass-winmenu -Recurse -Force
+
+$config = gc $installDir\pass-winmenu.yaml -Encoding UTF8
+if ($pp.PasswordStore) { $config = $config -replace '^password-store: .+', "password-store: $passwordStore" }
+$config | Set-Content $installDir\pass-winmenu.yaml -Encoding UTF8
+
+if (!(Test-Path $passwordStore)) {
+    Write-Host "Creating password store: $passwordStore"
+    mkdir $passwordStore -ea 0 | Out-Null
+}
+
+if ($pp.GpgId) { 
+    Write-Host "Setting up GpgId to $($pp.GpgId)"
+    $pp.GpgId | Out-File -Encoding utf8 $passwordStore\.gpg-id 
+}
+
+if ($running) { start $installDir\pass-winmenu.exe }
