4a2b839ad014ddca38065462ae986b233b3a0c2a
Miodrag Milic <miodrag.milic@gmail.com>
2017-09-26 12:29:02 +0200

    (gitlab-runner) added REGISTER_LOCKED to false
    Default is changed to true in v10.0

--- gitlab-runner/tools/register_example.ps1
+++ gitlab-runner/tools/register_example.ps1
@@ -4,18 +4,20 @@
     Running runners should automatically pick up config.toml changes.
 #>
 
+$Env:RUNNER_NAME                = ''
 $Env:REGISTRATION_TOKEN         = ''
 $Env:CI_SERVER_URL              = ''
-$Env:RUNNER_TAG_LIST            = ''
-
-$Env:RUNNER_NAME                = ''
-$Env:RUNNER_EXECUTOR            = 'shell'
-$Env:RUNNER_SHELL               = 'powershell'
+$Env:RUNNER_TAG_LIST            = ''        #comma separated list of tags
 
 $Env:CONFIG_FILE                = "$PSScriptRoot\config.toml"
 $Env:REGISTER_RUN_UNTAGGED      = 'false'
+$Env:REGISTER_LOCKED            = $false    #lock runner to current project 
+$Env:RUNNER_EXECUTOR            = 'shell'
+$Env:RUNNER_SHELL               = 'powershell'
+
 $Env:RUNNER_REQUEST_CONCURRENCY = 1
 $Env:RUNNER_BUILDS_DIR          = ''
 $Env:RUNNER_CACHE_DIR           = ''
+
 
 gitlab-runner register --non-interactive
