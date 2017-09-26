6ccb27690f85e7f27ccd1e1c8adb3fa67e1befa0
Miodrag Milic <miodrag.milic@gmail.com>
2017-09-26 11:59:18 +0200

    (gitlab-runner) update++
    gitlab-ci-multi-runner is renamed to gitlab-runner at vendor site
    Also added option to handle invalid semantic version 'rc.1' (contains dot)

--- gitlab-runner/update.ps1
+++ gitlab-runner/update.ps1
@@ -1,7 +1,6 @@
 import-module au
-. $PSScriptRoot\..\_scripts\all.ps1
 
-$releases = 'https://gitlab.com/gitlab-org/gitlab-ci-multi-runner/raw/master/CHANGELOG.md'
+$releases = 'https://gitlab.com/gitlab-org/gitlab-runner/raw/master/CHANGELOG.md'
 
 function global:au_SearchReplace {
    @{
@@ -14,17 +13,18 @@ function global:au_SearchReplace {
    }
 }
 
-function global:au_AfterUpdate  { Set-DescriptionFromReadme -SkipFirst 2 }
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
-
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $version = ($download_page.Content -split "`n") -match '^v (.+)$' | select -first 1
     $version = ($version.Substring(2) -replace '\(.+').Trim()
+
+    $cversion = $version
+    if ($version -match '-.+') { $cversion = ($cversion -replace '-.+') + $Matches[0].Replace('.', '')}
     @{
-        Version  = $version
-        URL32    = "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v${version}/binaries/gitlab-ci-multi-runner-windows-386.exe"
-        URL64    = "https://gitlab-ci-multi-runner-downloads.s3.amazonaws.com/v${version}/binaries/gitlab-ci-multi-runner-windows-amd64.exe"
+        Version  = $cversion
+        URL32    = "https://gitlab-runner-downloads.s3.amazonaws.com/v${version}/binaries/gitlab-runner-windows-386.exe"
+        URL64    = "https://gitlab-runner-downloads.s3.amazonaws.com/v${version}/binaries/gitlab-runner-windows-amd64.exe"
     }
 }
 
