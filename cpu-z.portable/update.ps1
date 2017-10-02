4ddc5c32d7f7260c6f06c440bb2dd0755ce3f6ac
Miodrag Milic <miodrag.milic@gmail.com>
2017-10-02 13:06:56 +0200

    (cpu-z*) removed au_BeforeUpdate; reverted Set-DescriptionFromReadme.ps1

--- cpu-z.portable/update.ps1
+++ cpu-z.portable/update.ps1
@@ -1,5 +1,4 @@
 import-module au
-. $PSScriptRoot\..\_scripts\all.ps1
 
 $releases = 'http://www.cpuid.com/softwares/cpu-z.htm'
 
@@ -11,8 +10,6 @@ function global:au_SearchReplace {
         }
     }
 }
-
-function global:au_AfterUpdate  { Set-DescriptionFromReadme -SkipFirst 2 }
 
 function global:au_GetLatest {
     $re      = 'cpu-z.+\.zip'
