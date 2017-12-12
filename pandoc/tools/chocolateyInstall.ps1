2007bc0747af0b896a770068e4e47e78818d3b3f
Miodrag Milic <miodrag.milic@gmail.com>
2017-12-12 19:47:08 +0100

    [AU pandoc] fixed bug with shim

--- pandoc/tools/chocolateyInstall.ps1
+++ pandoc/tools/chocolateyInstall.ps1
@@ -8,13 +8,14 @@ $packageArgs = @{
   file           = gi "$toolsDir\*.msi"
   silentArgs     = '/quiet'
   validExitCodes = @(0, 1223)
+  softwareName   = 'Pandoc *'
 }
 Install-ChocolateyInstallPackage @packageArgs
 rm ($toolsDir + '\*.' + $packageArgs.fileType)
 
 $packageName = $packageArgs.packageName
-$installLocation = Get-AppInstallLocation $packageName
-if (!$installLocation)  { Write-Warning "Can't find $PackageName install location"; return }
+$installLocation = Get-AppInstallLocation $packageArgs.softwareName
+if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
 
 Write-Host "$packageName installed to '$installLocation'"
 Install-BinFile $packageName $installLocation\pandoc.exe
