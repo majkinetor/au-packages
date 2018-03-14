f98dc307bc2ab417a2127fe8bbc92eec615b0bd5
Chocolatey <chocolatey@realdimensions.net>
2018-03-14 01:09:29 +0000

    AU: 2 updated - dbeaver-ee signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/94cbd6076ce56bef8a56848fc30ed52fb9ae356c

--- dbeaver-ee/tools/chocolateyInstall.ps1
+++ dbeaver-ee/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'dbeaver'
-$url32       = 'https://dbeaver.com/files/5.0.0/dbeaver-ee-5.0.0-x86-setup.exe'
-$url64       = 'https://dbeaver.com/files/5.0.0/dbeaver-ee-5.0.0-x86_64-setup.exe'
-$checksum32  = 'bdd18a54f38123562679912da3b8a5060e73ffa780f0612d60c3a20a0e80fbb7'
-$checksum64  = '05da9b458a3c9a4895efb3152a18ab41fb989d547a5452f986e3ec066ef512f5'
+$url32       = 'https://dbeaver.com/files/5.0.1/dbeaver-ee-5.0.1-x86-setup.exe'
+$url64       = 'https://dbeaver.com/files/5.0.1/dbeaver-ee-5.0.1-x86_64-setup.exe'
+$checksum32  = '00863ad7f4e4a97d6af73369216ab50af0a40c29bad9b10488f33f65880c42ca'
+$checksum64  = '8c3e4d1633929b53233ac305a568586b164bf7806d58f4022467a95dd12a02fa'
 
 $packageArgs = @{
   packageName            = $packageName
--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.5.2.exe'
-  checksum               = 'e0755c1acd4dd874e5a387e74ca03df9ff7d22543d8ebe74b92eb6ddc6a889d8'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.6.0.exe'
+  checksum               = '055d81d1bd497b86edfb31e29ff393298f0f3380cc7e482fe3fc23809d49fa40'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
