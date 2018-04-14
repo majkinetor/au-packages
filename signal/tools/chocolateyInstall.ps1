f8ff13a1ae6ae93953c9ce44bc504a4a333b4e50
Chocolatey <chocolatey@realdimensions.net>
2018-04-14 01:05:24 +0000

    AU: 3 updated - dngrep signal trid [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/bc6dc5a617ded6cd0f4494bdcf246f4705078c5e

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.7.0.exe'
-  checksum               = 'e7906286212f6a20d5870e91945e8df2958b333291ac3f5581c68af5499f7bf8'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.7.1.exe'
+  checksum               = '9247751004ee8f3181f475df64676046b5e93ea5e83ca969811730c5a9f6e85d'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
