ed4df2926d809c36d4ca99391126ebbd46c62d4c
Chocolatey <chocolatey@realdimensions.net>
2018-04-15 01:05:33 +0000

    AU: 1 updated - mls-software-openssh [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/debcbdaaffeedab4c987c82d122e5976564a691a

--- mls-software-openssh/tools/chocolateyInstall.ps1
+++ mls-software-openssh/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'mls-software-openssh'
-$url         = 'https://www.mls-software.com/files/setupssh-7.6p1-1.exe'
-$checksum    = '0f38977b9f270daedea66302f9067e07f75809371090747f0b0ca806dc626c89'
+$url         = 'https://www.mls-software.com/files/setupssh-7.7p1-1.exe'
+$checksum    = '5f70e0ef9a9886cabe66a943289792f7271ff4b84d6a3c1db2008fb5ba9b002d'
 
 $pp = Get-PackageParameters
 $params = @()
