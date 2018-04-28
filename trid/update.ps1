2d2852bb869419ec916e02a9cc7a0793e320056a
Miodrag Milic <miodrag.milic@gmail.com>
2018-04-28 08:24:32 +0200

    (trid) disabled

--- trid/update.ps1
+++ trid/update.ps1
@@ -50,6 +50,9 @@ function global:au_GetLatest {
     }
 }
 
+Write-Host 'Disabled for now as forum doesnt work'
+return 'ignore'
+
 sal 7z $env:ChocolateyInstall\tools\7z.exe
 mkdir $PSScriptRoot\tools -ea 0
 update -ChecksumFor none -NoCheckUrl
