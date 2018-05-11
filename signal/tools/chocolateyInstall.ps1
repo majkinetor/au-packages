a987f60d8d8392375461e3f2f8f0fda2b12430e0
Chocolatey <chocolatey@realdimensions.net>
2018-05-11 01:06:37 +0000

    AU: 3 updated - gitlab-runner rundeck signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/4d7cf3f79b5b9cf1093623ade9d18dfaf135c1f5

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.9.0.exe'
-  checksum               = 'cec4c83374847cdfcacb28cf95119e4e3d41e7fddd789f8413ab52a8fdc49ad8'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.10.0.exe'
+  checksum               = '4041f1904649911a6352559534b7c54d51a4c9f8ff73492111ada2f201738589'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
