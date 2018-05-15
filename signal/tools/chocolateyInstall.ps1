ace1be26a3a1f70cc483c742ccdc19ffe6a49f6f
Chocolatey <chocolatey@realdimensions.net>
2018-05-15 01:05:55 +0000

    AU: 2 updated - signal solr [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/64326e608d1b0355fd92a8a77553cf51aeb25116

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.10.1.exe'
-  checksum               = 'cfefe00e7304063014509f8e7a77cf99bde8ee812b30f0191b6cf8066ef2ece4'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.11.0.exe'
+  checksum               = 'a67d9f020c3196035c05a596729f70153ed4c992b9f97742aa85b930e1e3d47d'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
