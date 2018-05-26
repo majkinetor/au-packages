4b149e04e3f9c48f88525d2c169aed1dfcd1fbfc
Miodrag Milic <miodrag.milic@gmail.com>
2018-05-26 22:02:18 +0200

    [AU plantuml] refactoring; multiple java paths fix close #85

--- plantuml/tools/chocolateyInstall.ps1
+++ plantuml/tools/chocolateyInstall.ps1
@@ -1,56 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
-Update-SessionEnvironment   #java might have been installed
 
-$packageName = 'plantuml'
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
-$java_args   = '-Dfile.encoding=UTF-8 -jar "{0}"' -f "$toolsPath\plantuml.jar"
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+. $toolsPath\helper.ps1
 
-$javaw_path = (gcm javaw.exe -ea 0).Path
-$java_path  = (gcm java.exe -ea 0).Path
-if (!$javaw_path) { throw "javaw.exe is not on the PATH" }
-Write-Host "Using javaw: $javaw_path"
-Write-Host "Using java: $java_path"
+Update-SessionEnvironment   # Java might have been installed as dependency
+
+$java_path, $javaw_path = Get-JavaPaths
+$java_args = '-Dfile.encoding=UTF-8 -jar "{0}"' -f "$toolsPath\plantuml.jar"
+Write-Host "Java path: $(Split-Path $javaw_path)"
+Write-Host "Java args: $java_args"
 
 $pp = Get-PackageParameters
 
-# Create shortcut in tools directory for Register-Application
-$params = @{
-    ShortcutFilePath = "$toolsPath\plantuml.lnk"
-    TargetPath       = $javaw_path
-    Arguments        = $java_args
-    IconLocation     = "$toolsPath\plantuml.ico"
-}
-Install-ChocolateyShortcut @params
-
-Register-Application "$toolsPath\plantuml.lnk" plantuml
-Write-Host "$packageName registered as $packageName"
-
-# This binary is for interactive work (returns asap)
-$binparams = @{
-    name     = "plantuml"
-    path     = $javaw_path
-    useStart = $true
-    command  = """$java_args"""
-}
-Install-BinFile @binparams
-
-# This binary is for scripting, it waits for java to return
-$binparams = @{
-    name =  "plantumlc"
-    path = $java_path
-    useStart = $false
-    command = """$java_args"""
-}
-Install-BinFile @binparams
-
-if (!$pp.NoShortcuts) { 
-    Write-Host "Creating desktop shortcuts"
-    $params.ShortcutFilePath = "$Env:USERPROFILE\Desktop\Plantuml.lnk"
-    Install-ChocolateyShortcut @params 
-
-    $params = @{
-        ShortcutFilePath = "$Env:USERPROFILE\Desktop\Plantuml Reference.lnk"
-        TargetPath       = "$toolsPath\PlantUML_Language_Reference_Guide.pdf"
-    }
-    Install-ChocolateyShortcut @params
-}
+Install-PumlToolsShortcut
+Install-PumlBinaryW
+Install-PumlBinary
+if (!$pp.NoShortcuts) { Install-PumlDesktopShortcuts }
---
+++ plantuml/tools/helper.ps1
@@ -0,0 +1,68 @@
+function Get-JavaPaths {
+   if ($Env:JAVA_HOME -and (Test-Path $Env:JAVA_HOME\bin)) {
+        [array] $paths = (gi -Path $Env:JAVA_HOME\bin\* -ea 0 -Include java.exe,javaw.exe).FullName
+        if ($paths.Count -eq 2) { return $paths } 
+   }
+   $javaw_path = (gcm javaw.exe -ea 0).Path | select -First 1
+   $java_path  = (gcm java.exe -ea 0).Path  | select -First 1
+
+   if (!$javaw_path) { throw "javaw.exe is not on the PATH" }
+   
+   return $java_path, $javaw_path
+}
+
+# Create shortcut in tools directory for Register-Application
+function Install-PumlToolsShortcut {
+    $params = @{
+        ShortcutFilePath = "$toolsPath\plantuml.lnk"
+        TargetPath       = $javaw_path
+        Arguments        = $java_args
+        IconLocation     = "$toolsPath\plantuml.ico"
+    }
+    Install-ChocolateyShortcut @params
+
+    Register-Application "$toolsPath\plantuml.lnk" plantuml
+    Write-Host "Plantuml registered as plantuml"
+}
+
+function Install-PumlDesktopShortcuts {
+    Write-Host "Creating desktop shortcuts"
+
+    $params = @{
+        ShortcutFilePath = "$toolsPath\plantuml.lnk"
+        TargetPath       = $javaw_path
+        Arguments        = $java_args
+        IconLocation     = "$toolsPath\plantuml.ico"
+    }
+
+    $params.ShortcutFilePath = "$Env:USERPROFILE\Desktop\Plantuml.lnk"
+    Install-ChocolateyShortcut @params 
+
+    $params = @{
+        ShortcutFilePath = "$Env:USERPROFILE\Desktop\Plantuml Reference.lnk"
+        TargetPath       = "$toolsPath\PlantUML_Language_Reference_Guide.pdf"
+    }
+    Install-ChocolateyShortcut @params
+}
+
+# This binary is for interactive work (returns asap)
+function Install-PumlBinaryW {
+    $binparams = @{
+        name     = "plantuml"
+        path     = $javaw_path
+        useStart = $true
+        command  = """$java_args"""
+    }
+    Install-BinFile @binparams
+}
+
+# This binary is for scripting, it waits for java to return
+function Install-PumlBinary {
+    $binparams = @{
+        name     =  "plantumlc"
+        path     = $java_path
+        useStart = $false
+        command  = """$java_args"""
+    }
+    Install-BinFile @binparams
+}
