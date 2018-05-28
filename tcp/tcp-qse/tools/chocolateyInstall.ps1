147d3ef199bffb4e40368aaeb652501aedd9d305
Miodrag Milic <miodrag.milic@gmail.com>
2018-05-28 10:40:07 +0200

    .

--- tcp/tcp-qse/tools/chocolateyInstall.ps1
+++ tcp/tcp-qse/tools/chocolateyInstall.ps1
@@ -4,10 +4,9 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $is64      = (Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true'
 
 $packageArgs = @{
-    PackageName    = ''
-    FileFullPath   = $toolsPath\*_x32.zip
-    FileFullPath64 = $toolsPath\*_x64.zip    
-    Destination    = $toolsPath
+    PackageName    = 'tcp-qse'
+    FileFullPath   = gi $toolsPath\*.zip  
+    Destination    = $Env:COMMANDER_PATH
 }
 
 ls $toolsPath\* | ? { $_.PSISContainer } | rm -Recurse -Force #remove older package dirs
