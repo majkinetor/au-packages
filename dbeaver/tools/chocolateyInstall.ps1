1df71e1ba0b71bfa25de8769e0f1cfc605192d6c
Miodrag Milic <miodrag.milic@gmail.com>
2018-05-29 11:54:56 +0200

    (dbeaver) refactoring

--- dbeaver/tools/chocolateyInstall.ps1
+++ dbeaver/tools/chocolateyInstall.ps1
@@ -1,15 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir      = Split-Path $MyInvocation.MyCommand.Definition
-$embedded_path = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
-         Write-Host "Installing 64 bit version"; gi "$toolsDir\*_x64.exe"
-} else { Write-Host "Installing 32 bit version"; gi "$toolsDir\*_x32.exe" }
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = 'dbeaver'
   softwareName   = 'dbeaver*'
   fileType       = 'exe'
-  file           = $embedded_path
+  file           = gi "$toolsDir\*_x32.exe"
+  file64         = gi "$toolsDir\*_x64.exe"
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
