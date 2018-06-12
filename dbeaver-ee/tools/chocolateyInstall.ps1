9cf22e13c66196a2b251384a3d48975c35ca0151
Chocolatey <chocolatey@realdimensions.net>
2018-06-12 01:08:16 +0000

    AU: 3 updated - dbeaver-ee nirlauncher plantuml [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/17bcbd9733be61d6347ff88af422c2894b8f11e2

--- dbeaver-ee/tools/chocolateyInstall.ps1
+++ dbeaver-ee/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'dbeaver'
-$url32       = 'https://dbeaver.com/files/5.0.1/dbeaver-ee-5.0.1-x86-setup.exe'
-$url64       = 'https://dbeaver.com/files/5.0.1/dbeaver-ee-5.0.1-x86_64-setup.exe'
-$checksum32  = '00863ad7f4e4a97d6af73369216ab50af0a40c29bad9b10488f33f65880c42ca'
-$checksum64  = '8c3e4d1633929b53233ac305a568586b164bf7806d58f4022467a95dd12a02fa'
+$url32       = 'https://dbeaver.com/files/5.1.0/dbeaver-ee-5.1.0-x86-setup.exe'
+$url64       = 'https://dbeaver.com/files/5.1.0/dbeaver-ee-5.1.0-x86_64-setup.exe'
+$checksum32  = '9a7e2855dc9cf9e2793edc42bac66f13e150d59431a29aed8fa3419d5dd3ad4c'
+$checksum64  = '0531e0dd952eb893808a3ff84219c2b594b6159081e122b4aa503292b3fca509'
 
 $packageArgs = @{
   packageName            = $packageName
