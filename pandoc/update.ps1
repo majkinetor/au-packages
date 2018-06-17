564e350bca2b788878c3d4359d95fec2d01a2f00
Miodrag Milić <miodrag.milic@gmail.com>
2018-06-17 13:08:08 +0200

    Update update.ps1

--- pandoc/update.ps1
+++ pandoc/update.ps1
@@ -24,7 +24,7 @@ function global:au_SearchReplace() {
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_GetLatest() {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $re      = '/pandoc-(.+?)-windows.msi'
     $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
