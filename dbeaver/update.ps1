d1d451a1dd2cace409c88eea9966eba264123cc8
Miodrag Milić <miodrag.milic@gmail.com>
2018-06-17 12:09:55 +0200

    Update update.ps1

--- dbeaver/update.ps1
+++ dbeaver/update.ps1
@@ -27,7 +27,7 @@ function global:au_SearchReplace {
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $re    = '\.exe$'
     $url   = $download_page.links | ? href -match $re | select -Expand href -First 2 | % { 'https://github.com' + $_ }
