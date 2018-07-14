0bd6cc505b0192a4c0c69146909811cea949bead
Chocolatey <chocolatey@realdimensions.net>
2018-07-14 01:03:16 +0000

    AU: 2 updated - signal trid [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/a7327a09be5598d14b68396fc6f00e56d9e6b099

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.14.0.exe'
-  checksum               = '98e6c498da76ac9734f4ea708014a5f850ec173e6a6a64c126eb1a5ec00b7afd'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.14.1.exe'
+  checksum               = 'f2290e1d29a048df671174f399b1daf32a83e9974e516bf446974e4b38ea26e9'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
