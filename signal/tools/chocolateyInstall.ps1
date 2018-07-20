b2f7c3fe65471dbbd80192adcc732009323effb7
Chocolatey <chocolatey@realdimensions.net>
2018-07-20 01:03:44 +0000

    AU: 3 updated - signal tagscanner trid [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/c73c8c22d893871467d6d8de87a25176c51b1479

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.14.1.exe'
-  checksum               = 'f2290e1d29a048df671174f399b1daf32a83e9974e516bf446974e4b38ea26e9'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.14.3.exe'
+  checksum               = 'b173515bf402f6a565c96dbd79e6c04772f0e14857e269474c6174ed2671ac4e'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
