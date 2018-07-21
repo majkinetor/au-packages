83ef04f6bc9f1de35b8882014c253ea00a22792c
Chocolatey <chocolatey@realdimensions.net>
2018-07-21 01:02:35 +0000

    AU: 1 updated - signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/507983866af4d7920e3f2830dee114a7e2fe95ca

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.14.3.exe'
-  checksum               = 'b173515bf402f6a565c96dbd79e6c04772f0e14857e269474c6174ed2671ac4e'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.14.4.exe'
+  checksum               = 'bbadf2a8eb79b6a1cdbe3af44daac724e42d5af2cc81946d80619a36c6ebb336'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
