76a258aff98d47f89bdaabf489aafce84ac4bdcc
Chocolatey <chocolatey@realdimensions.net>
2018-08-09 01:03:43 +0000

    AU: 3 updated - gitlab-runner signal trid [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/4e67d44652ecd6613f532cf10e93cd497f742355

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.15.2.exe'
-  checksum               = 'e3146719e0303b5b9013815c70806502c395f71df18864476acd57d9f972c2ed'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.15.3.exe'
+  checksum               = '009a8392621f8c5c24d1bf6853cdee1157e74944ce34179db5a23755b03dc437'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
