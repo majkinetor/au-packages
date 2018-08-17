6227216fd31d56d06269238209bcc23ad4eae05e
Miodrag Milic <miodrag.milic@gmail.com>
2018-08-17 22:34:30 +0200

    [AU pandoc] update++ closes #92

--- pandoc/update.ps1
+++ pandoc/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://github.com/jgm/pandoc/releases'
+$releases = 'https://github.com/jgm/pandoc/releases/latest'
 
 function global:au_SearchReplace() {
     @{
@@ -15,8 +15,9 @@ function global:au_SearchReplace() {
 
         ".\legal\VERIFICATION.txt" = @{
           "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
           "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
-          "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL32)"
+          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
         }
     }
 }
@@ -26,11 +27,12 @@ function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 function global:au_GetLatest() {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re      = '/pandoc-(.+?)-windows.msi'
-    $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
-    $version = $Matches[1] -replace '-.+$'
+    $re      = '/pandoc-(.+?)-windows-.*.msi'
+    $url     = $download_page.links | ? href -match $re | select -First 2 -expand href
+    $version = $url[0] -split '/' | select -last 1 -skip 1
     @{
-        URL32        = 'https://github.com' + $url
+        URL32        = 'https://github.com' + ($url -match 'i386' | select -first 1)
+        URL64        = 'https://github.com' + ($url -notmatch 'i386' | select -first 1)
         Version      = $version
         ReleaseNotes = "https://github.com/jgm/pandoc/releases/tag/${version}"
     }
