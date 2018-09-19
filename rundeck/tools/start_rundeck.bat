24476d0cd625062b00613c9189ddc42d0de7f17a
Miodrag Milic <miodrag.milic@gmail.com>
2018-09-19 18:32:23 +0200

    rundeck v3 init

--- rundeck/tools/start_rundeck.bat
+++ rundeck/tools/start_rundeck.bat
@@ -1,6 +1,6 @@
 set CURDIR=%~dp0
-call %CURDIR%etc\profile.bat
-
 set RDECK_CLI_OPTS=-Xmx4096m -Xms1024m
 ::set RDECK_SSL_OPTS=
-java %RDECK_CLI_OPTS% %RDECK_SSL_OPTS% -Dfile.encoding=UTF-8 -jar rundeck-launcher.jar --skipinstall -d
+
+call %CURDIR%etc\profile.bat
+java %RDECK_CLI_OPTS% %RDECK_SSL_OPTS% -Dfile.encoding=UTF-8 -jar rundeck.war --skipinstall -d >> %CURDIR%\rundeck.log 2>&1
