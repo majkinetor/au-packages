008bf957cd2207486f7d874843e5f09ccc0465d8
Chocolatey <chocolatey@realdimensions.net>
2018-10-30 01:11:02 +0000

    AU: 4 updated - etcher nirlauncher signal visualsvnserver [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/f3b06680275c3a4fec25f4f4aa23c343e63754cb

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.17.1.exe'
-  checksum               = '262e10792c492dbe89b451f4b52100aacd7ea227dbd4817ad1c737e032e29580'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.17.2.exe'
+  checksum               = 'e3398664ae1d45710a73b98608da3643611892ef216ad03f9a93c9adc175f237'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
