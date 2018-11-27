23a755d5519cefd116760d91c5fc72d9059bb13b
Chocolatey <chocolatey@realdimensions.net>
2018-11-27 01:02:42 +0000

    AU: 3 updated - nirlauncher plantuml signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/501f4ec79f568b4f27a0e8672a59fe59f93ce99d

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.18.0.exe'
-  checksum               = '162d059d43aebc92b78194b52488351c713e3a3959682872bc07883cf93c51ec'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.18.1.exe'
+  checksum               = '8770346efde4829ae627d807fa605d59577e1d3c106c9979e7f873fe80aa9b4a'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
