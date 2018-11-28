08ebd9f0020e8399d26306538f0d9a9510d0c7e1
Chocolatey <chocolatey@realdimensions.net>
2018-11-28 01:02:10 +0000

    AU: 3 updated - cpu-z.install cpu-z.portable pandoc [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/b72b920bc90057f06f685cb0a0298e2961461230

--- cpu-z.portable/tools/chocolateyInstall.ps1
+++ cpu-z.portable/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'cpu-z.portable'
 $url32       = 'http://download.cpuid.com/cpu-z/cpu-z_1.87-en.zip'
 $url64       = $url32
-$checksum32  = '67775b3d159bab2d2d17731b432d9ae7451afc2a899799fc5917f2157c25d392'
+$checksum32  = 'fad8c570c17bad4dd9154105959d6f591a878ee8ebb3dfb44a4d9744449a875f'
 $checksum64  = $checksum32
 
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
