7c531dea57af9f32447d036115a6d32e4871113b
Miodrag Milic <miodrag.milic@gmail.com>
2019-01-19 01:45:36 +0100

    closes #102

---
+++ mkcert/README.md
@@ -0,0 +1,25 @@
+# [mkcert](https://chocolatey.org/packages/mkcert)
+
+`mkcert` is a simple tool for making locally-trusted development certificates. It requires no configuration.
+
+`mkcert` automatically creates and installs a local CA in the system root store, and generates locally-trusted certificates. mkcert does not automatically configure servers to use the certificates, though, that's up to you.
+
+```sh
+$ mkcert -install
+Created a new local CA at "/Users/filippo/Library/Application Support/mkcert" 💥
+The local CA is now installed in the system trust store! ⚡️
+The local CA is now installed in the Firefox trust store (requires restart)! 🦊
+
+$ mkcert example.com "*.example.org" myapp.dev localhost 127.0.0.1 ::1
+Using the local CA at "/Users/filippo/Library/Application Support/mkcert" ✨
+
+Created a new certificate valid for the following names 📜
+ - "example.com"
+ - "*.example.org"
+ - "myapp.dev"
+ - "localhost"
+ - "127.0.0.1"
+ - "::1"
+
+The certificate is at "./example.com+5.pem" and the key at "./example.com+5-key.pem" ✅
+```
---
+++ mkcert/update.ps1
@@ -0,0 +1,38 @@
+import-module au
+
+$releases = 'https://github.com/FiloSottile/mkcert/releases/latest'
+
+function global:au_SearchReplace {
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { 
+    Get-RemoteFiles -Purge 
+    mv tools\mkcert*.exe tools\mkcert.exe
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re    = '\.exe$'
+    $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version  = $url -split '-|.exe' | select -Last 1 -Skip 3
+
+    @{
+        Version      = $version.Substring(1)
+        URL64        = 'https://github.com/' + $url
+        ReleaseNotes = 'https://github.com/FiloSottile/mkcert/releases/tag/$version'
+    }
+}
+
+update -ChecksumFor none
