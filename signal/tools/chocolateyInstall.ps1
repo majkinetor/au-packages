4ffd08ecc08b0e6d4ef4eb6380876ab24a67a6ed
Chocolatey <chocolatey@realdimensions.net>
2019-02-09 01:03:21 +0000

    AU: 3 updated - dbeaver pandoc signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/94b3ee5ff7d879687e35e82a6fb2aee28013ce6f

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.21.0.exe'
-  checksum               = 'dcac94ab8668ffba785b9b08c477b3d7ea7b15ea432462def93d78dc7fdcf116'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.21.1.exe'
+  checksum               = 'adc1b09a07f72f9fef203af84ba92c2a8334b203426bbbf224c94d68cbf153fd'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
