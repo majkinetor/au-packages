3121bd6447e061051a34d7be75eaeaf773af7ed2
Miodrag Milic <miodrag.milic@gmail.com>
2019-05-28 21:56:41 +0200

    (etcher) update++

--- etcher/tools/chocolateyInstall.ps1
+++ etcher/tools/chocolateyInstall.ps1
@@ -3,12 +3,9 @@
 $packageArgs = @{
   packageName            = 'etcher'
   fileType               = 'exe'
-  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.39/balenaEtcher-Setup-1.5.39-x86.exe'
-  url64bit               = 'https://github.com/balena-io/etcher/releases/download/v1.5.39/balenaEtcher-Setup-1.5.39-x64.exe'
-  checksum               = '1cf7b2edafe0b13070faef80b1143351848080a833a8174a31bde6b9d539a373'
-  checksum64             = 'deb5e27c9b655b792f0f2d07d396df53083410cc60984771df13fd1952538496'
+  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.43/balenaEtcher-Setup-1.5.43.exe'
+  checksum               = ''
   checksumType           = 'sha256'
-  checksumType64         = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
   softwareName           = 'etcher*'
--- etcher/update.ps1
+++ etcher/update.ps1
@@ -6,9 +6,7 @@ function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
             "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
             "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
             "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
@@ -20,13 +18,12 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $re    = 'Setup.+\.exe$'
-    $url   =  $download_page.links | ? href -match $re | select -First 2 |  % { "https://github.com" + $_.href }
-    $version  = $url[0] -split '-' | select -Last 1 -Skip 1
+    $url   =  $download_page.links | ? href -match $re | select -First 1 | % { "https://github.com" + $_.href }
+    $version  = $url -split '/' | select -Last 1 -Skip 1
 
     @{
-        Version      = $version
-        URL32        = $url -notmatch 'x64' | select -First 1
-        URL64        = $url -match 'x64' | select -First 1
+        Version = $version.Substring(1)
+        URL32   = $url
     }
 }
 
