9df19928fe9c94e3d9437c32d9c429fe995afa9d
Chocolatey <chocolatey@realdimensions.net>
2019-06-04 01:02:18 +0000

    AU: 3 updated - etcher gitlab-runner nirlauncher [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/58730139880dbb7183988632cd9bcaa5de486920

--- etcher/tools/chocolateyInstall.ps1
+++ etcher/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'etcher'
   fileType               = 'exe'
-  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.43/balenaEtcher-Setup-1.5.43.exe'
-  checksum               = 'fc6a5eddb29bc5770bc4413e6e9c0eb957f3f3095ff41ab9751622438bd24cef'
+  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.44/balenaEtcher-Setup-1.5.44.exe'
+  checksum               = '4e29868ccc522d45449f9f31bd188539d5040184279fa04321183857bd6b0497'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
