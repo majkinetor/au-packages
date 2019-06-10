6d2c5eeb5ea22aa4778522b33f7be22fd7216ade
Chocolatey <chocolatey@realdimensions.net>
2019-06-10 01:02:06 +0000

    AU: 3 updated - dbeaver etcher pass-winmenu [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/9309d925f421107a032ec19dea54445a9b0be3b3

--- etcher/tools/chocolateyInstall.ps1
+++ etcher/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'etcher'
   fileType               = 'exe'
-  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.45/balenaEtcher-Setup-1.5.45.exe'
-  checksum               = '020fd740cca17af3b184c28975b806de4b57bed41ff462e16d501170ba7b94d2'
+  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.46/balenaEtcher-Setup-1.5.46.exe'
+  checksum               = 'bc028c95782a0cdcdf47b34dc303a83e04b7a9212f35329cba77c3c8ef0f8420'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
