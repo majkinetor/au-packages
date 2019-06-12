2be16e2f9a8372c5e20cdfd457b30cfc6d7daf8f
Chocolatey <chocolatey@realdimensions.net>
2019-06-12 15:19:45 +0000

    AU: 2 updated - etcher pandoc [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/b8fb05e1946dfa235822ef4e02eff3eedef4d2af

--- etcher/tools/chocolateyInstall.ps1
+++ etcher/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'etcher'
   fileType               = 'exe'
-  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.46/balenaEtcher-Setup-1.5.46.exe'
-  checksum               = 'bc028c95782a0cdcdf47b34dc303a83e04b7a9212f35329cba77c3c8ef0f8420'
+  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.47/balenaEtcher-Setup-1.5.47.exe'
+  checksum               = '9f53fbae0e50fc84c8946057683408a8d142dc73de4930258bb587b6eed9a36d'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
