a98b0ba37f3c2d962f555eb2108b67fdd336a3f4
Chocolatey <chocolatey@realdimensions.net>
2019-06-14 01:02:36 +0000

    AU: 2 updated - etcher trid [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/5c2d2b26ee65ac48733ede464d208f9d3589e712

--- etcher/tools/chocolateyInstall.ps1
+++ etcher/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'etcher'
   fileType               = 'exe'
-  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.47/balenaEtcher-Setup-1.5.47.exe'
-  checksum               = '9f53fbae0e50fc84c8946057683408a8d142dc73de4930258bb587b6eed9a36d'
+  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.49/balenaEtcher-Setup-1.5.49.exe'
+  checksum               = 'f4f1c9f337b1cc859e2ce2c3a5eed98bea5bfa29927284acb4c75952aa207198'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
