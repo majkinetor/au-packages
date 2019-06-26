f90647e32f1545bc67c7e14a3b0a1499c05138da
Chocolatey <chocolatey@realdimensions.net>
2019-06-26 01:02:51 +0000

    AU: 3 updated - cpu-z cpu-z.install signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/658f25fcf6f6eea8f46b73984fe805996923c63d

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.25.1.exe'
-  checksum               = '5674701fe7e60bbb14c6292f3900a0294824217ec4968e75cfe26316791417fd'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.25.2.exe'
+  checksum               = '8ada1a5b68d89b674d6cf683d9477f1bbce363960482f7666635174c8a7235c1'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
