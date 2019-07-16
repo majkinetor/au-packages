7eb2ac93965a7eb2451ba63afa049956b0ce1460
Miodrag Milić <miodrag.milic@gmail.com>
2019-07-16 13:09:05 +0200

    [AU influxdb] fix package parameter

--- influxdb1/tools/chocolateyInstall.ps1
+++ influxdb1/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@ $ErrorActionPreference = 'Stop'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $pp = Get-PackageParameters
-if (!$pp.InstallDir) { $pp.InstallDir = 'C:\influxdata' }
+if (!$pp.InstallRoot) { $pp.InstallRoot = 'C:\influxdata' }
 
 $packageArgs = @{
     PackageName    = 'influxdb1'
     FileFullPath64 = gi $toolsPath\*.zip    
-    Destination    = $pp.InstallDir
+    Destination    = $pp.InstallRoot
 }
 
 ls $toolsPath\* | ? { $_.PSISContainer } | rm -Recurse -Force #remove older package dirs
