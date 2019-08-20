e0436af65563ec77880e361e8a5a51ee72aa56db
Chocolatey <chocolatey@realdimensions.net>
2019-08-20 01:01:46 +0000

    AU: 1 updated - etcher [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/ee88295383db428a315e4f0f17d940d440e93910

--- etcher/tools/chocolateyInstall.ps1
+++ etcher/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'etcher'
   fileType               = 'exe'
-  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.53/balenaEtcher-Setup-1.5.53.exe'
-  checksum               = '4e46323834cdfd60e5d333caf570cf6e46de4e47ee572085c307c36afcc7b7e6'
+  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.54/balenaEtcher-Setup-1.5.54.exe'
+  checksum               = 'a29c2e750a0dbade8ac37666d0c1064af8d7813e31ba0596f98c3d60e2f7959d'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
