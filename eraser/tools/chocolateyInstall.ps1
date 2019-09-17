934487db8b2b93a550cda26ee6a9cf8b201a0fdc
Miodrag Milic <miodrag.milic@gmail.com>
2019-09-17 20:28:34 +0200

    [AU eraser:6.2.0.2986] embedded; updater closes #119

--- eraser/tools/chocolateyInstall.ps1
+++ eraser/tools/chocolateyInstall.ps1
@@ -1,32 +1,25 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'eraser'
-$url         = 'https://sourceforge.net/projects/eraser/files/Eraser%206/6.2/Eraser%206.2.0.2986.exe'
-$checksum    = 'E284DC391BC979CC0361D0651FB6EE5DE990810C7694E50694E9AB9FC7D44741'
-
-$setupDir    = Get-PackageCacheLocation
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$is64      = (Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true'
+$setupDir  = Get-PackageCacheLocation
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url
-  url64Bit       = $url
-  checksum       = $checksum
-  checksum64     = $checksum
-  checksumType   = 'sha256'
-  checksumType64 = 'sha256'
-  unzipLocation  = $setupDir
+    PackageName    = $Env:ChocolateyPackageName 
+    FileFullPath   = gi $toolsPath\*.exe
+    FileFullPath64 = gi $toolsPath\*.exe    
+    Destination    = $setupDir
 }
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs
+ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" "" }}
 
-$msi_path = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
-    ls "$setupDir\*eraser*x64*.msi" -Recurse | select -Expand FullName
-} else {
-    ls "$setupDir\*eraser*x86*.msi" -Recurse | select -Expand FullName
-}
+
+$msi_glob = if ($is64) { "*eraser*x64*.msi" } else { "*eraser*x86*.msi"}
+$msi_path = ls "$setupDir\$msi_glob" -Recurse | select -Expand FullName
 if (!(Test-Path $msi_path)) { throw "Unpacking failed" }
 
 $packageArgs = @{
-  packageName    = $packageName
+  packageName    = $Env:ChocolateyPackageName 
   fileType       = 'msi'
   file           = $msi_path
   silentArgs     = '/quiet'
@@ -37,6 +30,6 @@ Install-ChocolateyInstallPackage @packageArgs
 rm -force -r $setupDir\eraser\* -ea 0
 
 $installLocation = Get-AppInstallLocation $packageArgs.softwareName
-if (!$installLocation) { Write-Warning "Can't find $packageName install location"; return }
-Write-Host "$packageName installed to '$installLocation'"
+if (!$installLocation) { Write-Warning "Can't find install location"; return }
+Write-Host "Installed to '$installLocation'"
 
---
+++ eraser/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+$releases = 'https://eraser.heidi.ie/download'
+
+function global:au_SearchReplace {
+   @{
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"     = "`${1} $($Latest.URL32)"
+          "(?i)(checksum32:).*" = "`${1} $($Latest.Checksum32)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix -FileNameBase "Eraser $($Latest.Version)" }
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re      = '\.exe/download$'
+    $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $version = $url -split '%20|.exe' | select -Last 1 -Skip 1
+
+    @{
+        Version      = $version
+        URL32        = $url
+        FileType     = 'exe'
+    }
+}
+
+update -ChecksumFor none
