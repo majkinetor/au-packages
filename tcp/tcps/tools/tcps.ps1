6542196ea12cf14134ce8d2026603071a343e1b4
Miodrag Milic <miodrag.milic@gmail.com>
2019-10-09 21:23:05 +0200

    (tcps) fix choco related tcps funcs

--- tcp/tcps/tools/tcps.ps1
+++ tcp/tcps/tools/tcps.ps1
@@ -4,38 +4,23 @@
 if (!$Env:COMMANDER_PLUGINS_PATH) { $Env:COMMANDER_PLUGINS_PATH = Join-Path (Get-ToolsLocation) TCPlugins }
 mkdir -ea 0 $Env:COMMANDER_PLUGINS_PATH
 
-$is32 = if ($Env:ChocolateyPackageName) { (Get-ProcessorBits 32) -or $env:chocolateyForceX86 -eq 'true' } else { $false }
+$TCP_Types = 'Wcx', 'Wdx', 'Wfx', 'Wlx'
 
-$TCP_Types      = 'Wcx', 'Wdx', 'Wfx', 'Wlx'
-$TCP_PluginType = ''
-$TCP_PluginFile = ''
- 
-function Get-TCPluginInfo {
+function Get-TCPluginPath {
     param(
         [string] $Name,
-        [string] $PluginsPath = $Env:COMMANDER_PLUGINS_PATH, 
-        [switch] $x32
+        [string] $PluginsPath = $Env:COMMANDER_PLUGINS_PATH
     )
-    
-    $pluginExtensions = $TCP_Types | % { if ($x32) {"*.$_"} else {"*.${_}64"} }
-    $global:TCP_PluginFile = Get-ChildItem -Recurse -Path $PluginsPath -Include $pluginExtensions -Filter "$Name.*" 
-    if (!$global:TCP_PluginFile) { throw "Can't find plugin installation for the '$Name'" }
-    if ($global:TCP_PluginFile.Count -gt 1) { throw "Multiple plugins found by the name '$Name': $($global:TCP_PluginFile.Name)"  } 
-    $type = $global:TCP_PluginFile.Extension.Substring(1) -replace '64'
-    $global:TCP_PluginType = $type.Substring(0,1).ToUpper() + $type.Substring(1)
-}
+    $is32 = if ((Get-ProcessorBits 32) -or $env:chocolateyForceX86 -eq 'true') { $true } else { $false }
 
-function Get-CallingPackageToolsDir
-{
-    if ($toolsPath) { return $toolsPath }
-
-    $cStack = @(Get-PSCallStack)
-    Split-Path $cStack[$cStack.Length-3].InvocationInfo.MyCommand.Source
+    $pluginExtensions = $TCP_Types | % { if ($is32) {"*.$_"} else {"*.${_}64"} }
+    $pluginFile = Get-ChildItem -Recurse -Path $PluginsPath -Include $pluginExtensions -Filter "$Name.*" 
+    if (!$pluginFile) { throw "Can't find plugin installation for the '$Name'" }
+    if ($pluginFile.Count -gt 1) { throw "Multiple plugins found by the name '$Name': $($pluginFile.Name)"  }
+    return $pluginFile.FullName
 }
 
 function Install-TCPlugin($Name, $DetectString, $ArchiveExt) {
-    $toolsPath = Get-CallingPackageToolsDir
-
     $packageArgs = @{
         PackageName    = $Env:ChocolateyPackageName 
         FileFullPath   = gi $toolsPath\*$Name* -Include '*.rar','*.zip'
@@ -43,24 +28,22 @@ function Install-TCPlugin($Name, $DetectString, $ArchiveExt) {
         Destination    = "$Env:COMMANDER_PLUGINS_PATH\$Name"
     }
     Get-ChocolateyUnzip @packageArgs
-    if ($Env:ChocolateyPackageName) {  Remove-Item $packageArgs.FileFullPath -ea 0 }
+    Remove-Item $packageArgs.FileFullPath -ea 0
 
-    Get-TCPluginInfo $Name -x32:$is32
-
+    $pluginPath = Get-TCPluginInfo $Name
     if (Test-DC) {
         Write-Host "Adding plugin settings for Double Commander"
         Close-DC
-        Set-DCPlugin $TCP_PluginFile.FullName -DetectString $DetectString -ArchiveExt $ArchiveExt
+        Set-DCPlugin $pluginPath -DetectString $DetectString -ArchiveExt $ArchiveExt
     }
     if (Test-TC) {
         Write-Host "Adding plugin settings for Total Commander"
         CLose-TC
-        Set-TCPlugin $TCP_PluginFile.FullName -DetectString $DetectString -ArchiveExt $ArchiveExt
+        Set-TCPlugin $pluginPath -DetectString $DetectString -ArchiveExt $ArchiveExt
     }
 }
 
 function Uninstall-TCPlugin($Name) {
-    Get-TCPluginInfo $Name -x32:$is32
 
     if (Test-DC) {
         Write-Host "Removing plugin settings for Double Commander"
