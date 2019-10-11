fb9ac565f2cda7c14afa7dcd6b0aba57c9269985
Miodrag Milic <miodrag.milic@gmail.com>
2019-10-11 21:30:24 +0200

    .

--- tcp/tcp-mediainfo/tools/chocolateyUninstall.ps1
+++ tcp/tcp-mediainfo/tools/chocolateyUninstall.ps1
@@ -4,6 +4,3 @@
 
 Uninstall-TCPlugin TCMediaInfo -NoRemove
 Uninstall-TCPlugin TCMediaInfo -ForceType Wlx
-# Above will remove content plugin
-# Can't remove lister plugin settings in TC & DC due to tcps limitations
-# However, leftover settings for missing plugin wont create any problems.
