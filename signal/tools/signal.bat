52fd76087d049cfde9f7d6b89593764ba973a2d6
Miodrag Milić <miodrag.milic@gmail.com>
2019-10-24 20:41:49 +0100

    (signal) fix bug with space in username
    closes #131

--- signal/tools/signal.bat
+++ signal/tools/signal.bat
@@ -2,4 +2,4 @@ set HTTP_PROXY=
 set HTTPS_PROXY=%HTTP_PROXY%
 
 ::set SIGNAL_OPTIONS=--use-tray-icon --start-in-tray
-start %LOCALAPPDATA%\Programs\signal-desktop\Signal.exe %SIGNAL_OPTIONS%
+start "" "%LOCALAPPDATA%\Programs\signal-desktop\Signal.exe" %SIGNAL_OPTIONS%
