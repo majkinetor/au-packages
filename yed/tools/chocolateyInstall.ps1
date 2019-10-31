91543629d4e93cf416d6ad2b46a2f195dd7d0511
Chocolatey <chocolatey@realdimensions.net>
2019-10-31 01:02:03 +0000

    AU: 2 updated - trid yed [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/95dbd2502e275d035569427962574903936d574d

--- yed/tools/chocolateyInstall.ps1
+++ yed/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ rm $toolsDir\yed-* -Recurse -ea 0
 
 $packageArgs = @{
   packageName    = 'yed'
-  url            = 'https://www.yworks.com/resources/yed/demo/yEd-3.19.1.zip'
-  url64bit       = 'https://www.yworks.com/resources/yed/demo/yEd-3.19.1.zip'
-  checksum       = '19ccb355bd22702e4e1d2dbb976e21acb2d8baeca4c9d4a3574e5876f8fd3308'
-  checksum64     = '19ccb355bd22702e4e1d2dbb976e21acb2d8baeca4c9d4a3574e5876f8fd3308'
+  url            = 'https://www.yworks.com/resources/yed/demo/yEd-3.19.1.1.zip'
+  url64bit       = 'https://www.yworks.com/resources/yed/demo/yEd-3.19.1.1.zip'
+  checksum       = '3639da2650976bffcd43a8ea830727bae86478e95ceb4d68ad6363a42a8f1efb'
+  checksum64     = '3639da2650976bffcd43a8ea830727bae86478e95ceb4d68ad6363a42a8f1efb'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
