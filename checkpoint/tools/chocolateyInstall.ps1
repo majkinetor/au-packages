36f5d9ad41a802ebc1f883b5d3ece023577f91ff
Chocolatey <chocolatey@realdimensions.net>
2019-11-12 10:13:24 +0000

    AU: 1 updated - checkpoint [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/f197d235d429c6c7c4b66692a2aadb9bf86eee50

--- checkpoint/tools/chocolateyInstall.ps1
+++ checkpoint/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName            = $Env:ChocolateyPackageName
   fileType               = 'MSI'
-  url                    = 'http://dl3.checkpoint.com/paid/5f/5f8185de35b0cd83fe7480d891fe8df6/E82.00_CheckPointVPN.msi?HashKey=1573560171_f4e2774d72b7a7e38f235dbf93096ad4&xtn=.msi'
+  url                    = 'http://dl3.checkpoint.com/paid/5f/5f8185de35b0cd83fe7480d891fe8df6/E82.00_CheckPointVPN.msi?HashKey=1573560746_011156b7e2a0c68058182c2f9f5b3a6d&xtn=.msi'
   checksum               = '24fb5824d23b59494197214b75309cb58759ea11ac91aea2cf05628ae7ab57af'
   checksumType           = 'sha256'
   silentArgs             = '/q /log "{0}/setup.log"' -f "$Env:TEMP\chocolatey\$Env:ChocolateyPackageName"
