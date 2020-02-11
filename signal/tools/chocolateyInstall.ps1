85310dbb995b4fc990d63a897fec2971dd5cf842
Chocolatey <chocolatey@realdimensions.net>
2020-02-11 01:03:31 +0000

    AU: 2 updated - signal trid [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/8ce0fb683c8c6b502af00b9e895d40d88f7409a0

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ if ($pp.NoAutoUpdate) { Set-AutoUpdate -Enable }
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.30.1.exe'
-  checksum               = '43bb2c006a51899c2a11fc27d901814cdd4257b6289ee1a5f1c0955112233cd0'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.31.0.exe'
+  checksum               = 'f18332551c1407e8a01f73c227ea88cc967c1d286b77139070734d86ddf7a615'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
