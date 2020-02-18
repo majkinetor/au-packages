d04d741c892402163b78b0f4291eddb8728fba8b
Chocolatey <chocolatey@realdimensions.net>
2020-02-18 01:21:19 +0000

    AU: 3 updated - etcher nirlauncher trid [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/45837c04b464ad47ccfce30c486d24ef4a3c7f0a

--- etcher/tools/chocolateyInstall.ps1
+++ etcher/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'etcher'
   fileType               = 'exe'
-  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.76/balenaEtcher-Setup-1.5.76.exe'
-  checksum               = '8dc7bdc1bdb92f082f2402cf07625031c8d182245560ae3d38992e5d834a2f63'
+  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.77/balenaEtcher-Setup-1.5.77.exe'
+  checksum               = '0ee5ae5f7508675cf50f7d94e491adab4e1cf30451551aac215efdf12afdba9a'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
