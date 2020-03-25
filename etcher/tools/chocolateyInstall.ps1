31beebd2483e296d56e9ca9fb72be866940e0710
Chocolatey <chocolatey@realdimensions.net>
2020-03-25 01:03:51 +0000

    AU: 4 updated - dbeaver etcher rundeck shutup10 [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/ffc7252e3057fcafaa403bf58c502570987afe14

--- etcher/tools/chocolateyInstall.ps1
+++ etcher/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'etcher'
   fileType               = 'exe'
-  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.78/balenaEtcher-Setup-1.5.78.exe'
-  checksum               = 'd89c279b380e89cfb0ed0779ef2023fb42e45f3e5285bb1a8d83c3e9541edb73'
+  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.80/balenaEtcher-Setup-1.5.80.exe'
+  checksum               = '4d6a20dbf34816e02d51a9ce280038f11001d08bd9613aa8c17c74b704461022'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
