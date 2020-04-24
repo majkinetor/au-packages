08a0c79a3dff3fbd007cb136efd6346e6f4c26ff
Chocolatey <chocolatey@realdimensions.net>
2020-04-24 01:02:54 +0000

    AU: 2 updated - signal visualsvnserver [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/4e97306a2507095e3b38046239b089eb55888a90

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ if ($pp.NoAutoUpdate) { Set-AutoUpdate -Enable }
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.33.0.exe'
-  checksum               = '11a02e17e93e998223dc039e619db76dc31f71996ad0b214999adb48e508d97b'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.33.3.exe'
+  checksum               = 'e0622ba5a6c28b6fa58c135b85cb38153ac2126300e7cc5cda051c5faffe9c83'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
