1665955f300e4b5ca42c7495b6d07a03497169c3
Chocolatey <chocolatey@realdimensions.net>
2020-05-13 01:02:24 +0000

    AU: 3 updated - etcher shutup10 trid [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/7b285552e74103bafb67e166ebda7790e53d2a01

--- etcher/tools/chocolateyInstall.ps1
+++ etcher/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'etcher'
   fileType               = 'exe'
-  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.82/balenaEtcher-Setup-1.5.82.exe'
-  checksum               = '06bddd2c36f82ef24bbade213bd8685d69d5f53413022fce006433016618f9d3'
+  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.88/balenaEtcher-Setup-1.5.88.exe'
+  checksum               = '96960036a9b8dccbeed2f6e0b8950af9c0ec8f06f3ce3376b72110d38a49a02c'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
