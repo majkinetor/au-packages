99e18d24d9705543ac104f1abe4b351da0428cde
teknowledgist <teknowledgist@users.noreply.github.com>
2020-06-15 08:38:57 -0400

    Update chocolateyinstall.ps1

--- solr/tools/chocolateyinstall.ps1
+++ solr/tools/chocolateyinstall.ps1
@@ -3,19 +3,15 @@ $ErrorActionPreference = 'Stop'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $pp = Get-PackageParameters
-if ($pp.Path) {
+if (!$pp.Path) {
+   $UnzipPath = Get-ToolsLocation
+   Write-Host "Solr will be unpacked to the default location, '$UnzipPath'."
+} else {
    # make sure the path string is valid
    $null = [system.io.fileinfo]($pp.Path)
    Write-Host "Solr will be unpacked to the custom location, '$($pp.Path)'."
    Write-Warning 'Custom locations are not identified or cleared on Chocolatey package upgrade or uninstall.'
-   if (-not (Test-Path $pp.Path)) {
-      Write-Host "Creating destination directory, '$($pp.Path)'"
-      $null = New-Item -Path $pp.Path -ItemType Directory
-   }
    $UnzipPath = $pp.Path
-} else {
-   $UnzipPath = Get-ToolsLocation
-   Write-Host "Solr will be unpacked to the default location, '$UnzipPath'."
 }
 
 $packageArgs = @{
