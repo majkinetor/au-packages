56006bed4c8fbcfd651d0a6a710728b2853a48fd
Chocolatey <chocolatey@realdimensions.net>
2020-08-27 01:01:35 +0000

    AU: 2 updated - etcher rundeck-cli [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/5f40634906adc84422eb7d1853b809c5cafcac1b

--- etcher/tools/chocolateyInstall.ps1
+++ etcher/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'etcher'
   fileType               = 'exe'
-  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.104/balenaEtcher-Setup-1.5.104.exe'
-  checksum               = '51568efb48579c498ecd7fec2243baaefe227b7bdf6682b0461128c8d080add0'
+  url                    = 'https://github.com/balena-io/etcher/releases/download/v1.5.105/balenaEtcher-Setup-1.5.105.exe'
+  checksum               = '5735ad788e50a7254a47a9e2cfc3e5decffeb3a9060a32f0990b864769a6f3be'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
