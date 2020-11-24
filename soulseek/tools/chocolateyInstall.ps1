3391c736d70928f6ff19f087c7764adf7f4dcbec
Miodrag Milic <miodrag.milic@gmail.com>
2020-11-24 11:44:37 +0100

    (soulseek) manually add x64 bit latest version

--- soulseek/tools/chocolateyInstall.ps1
+++ soulseek/tools/chocolateyInstall.ps1
@@ -1,41 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-function download-dropbox($Url, $FilePath) {
-    $wc = New-Object system.net.webclient
-    $req = [System.Net.HttpWebRequest]::Create($Url)
-    $req.CookieContainer  = New-Object System.Net.CookieContainer
-    $res = $req.GetResponse()
-
-    $cookies = $res.Cookies | % { $_.ToString()}
-    $cookies = $cookies -join '; '
-
-    $wc.Headers.Add([System.Net.HttpRequestHeader]::Cookie, $cookies)
-    $newurl = $url + '?dl=1'
-
-    mkdir (Split-Path $FilePath) -force -ea 0 | out-null
-    $wc.downloadFile($newurl, $tempFile)
-}
-
-$packageName  = 'soulseek'
-$url32        = 'https://www.slsknet.org/SoulseekQt/Windows/SoulseekQt-2018-11-16-32bit.exe'
-$checksum32   = '2bb1776b8665c3c50af54c1159c0cc5c3e8bbef0a387a4502de5ba42e533fbf3'
-
-$chocoTempDir = Join-Path $Env:Temp "chocolatey"
-$tempFile     = "$chocoTempDir\soulseek\soulseek.exe"
-download-dropbox $url32 $tempFile
+$p = Get-Process SoulseekQt -ea 0
+if ($p) { Write-Host "Stopping running process"; $path = $p.Path; Stop-Process $p }
 
 $packageArgs = @{
-  packageName            = $packageName
+  packageName            = $Env:ChocolateyPackageName
   fileType               = 'EXE'
-  url                    = $tempFile
-  checksum               = $checksum32
+  url64bit               = 'https://www.slsknet.org/SoulseekQt/Windows/SoulseekQt-2019-7-22-64bit.exe'
+  checksum               = 'f72eb1f5581cc460e1cac912aac998e023b52b7bd365d888e59293cec63742ac'
   checksumType           = 'SHA256'
   silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes         = @(0)
-  registryUninstallerKey = $packageName
+  registryUninstallerKey = 'SoulseekQt*'
 }
 Install-ChocolateyPackage @packageArgs
 
 $installLocation = Get-AppInstallLocation $packageArgs.registryUninstallerKey
 if ($installLocation)  { Write-Host "$packageName installed to '$installLocation'" }
 else { Write-Warning "Can't find $PackageName install location" }
+
+if ($path) { Write-Host "Restarting Soulseek"; & $path }
