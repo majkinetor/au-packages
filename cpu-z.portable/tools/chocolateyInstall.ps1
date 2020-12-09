497c69c08fd947fa2faea2aa6c0b2581dd4f61b4
Miodrag Milic <miodrag.milic@gmail.com>
2020-12-09 13:16:42 +0100

    [AU cpu-z.portable]
    #172

--- cpu-z.portable/tools/chocolateyInstall.ps1
+++ cpu-z.portable/tools/chocolateyInstall.ps1
@@ -1,34 +1,26 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'cpu-z.portable'
-$url32       = 'http://download.cpuid.com/cpu-z/cpu-z_1.94-en.zip'
-$url64       = $url32
-$checksum32  = '5ecd8a890e4288a4c304c37c911aae5e8f2310df1f19b56100bdbb6da9963120'
-$checksum64  = $checksum32
-
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$is64      = (Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true'
 
 $packageArgs = @{
-  packageName            = $packageName
-  url                    = $url32
-  url64bit               = $url64
-  checksum               = $checksum32
-  checksum64             = $checksum64
-  checksumType           = 'sha256'
-  checksumType64         = 'sha256'
-  unzipLocation          = $toolsPath
+  packageName   = $Env:ChocolateyPackageName
+  url           = 'https://download.cpuid.com/cpu-z/cpu-z_1.94-en.zip'
+  checksum      = '5ecd8a890e4288a4c304c37c911aae5e8f2310df1f19b56100bdbb6da9963120'
+  checksumType  = 'sha256'
+  unzipLocation = $toolsPath
 }
 
 Install-ChocolateyZipPackage @packageArgs
-if (Get-ProcessorBits 64) {
+if ($is64) {
     Remove-Item $toolsPath\cpuz_x32.exe
     Move-Item -force $toolsPath\cpuz_x64.exe $toolsPath\cpuz.exe
 } else {
     Remove-Item $toolsPath\cpuz_x64.exe
     Move-Item -force $toolsPath\cpuz_x32.exe $toolsPath\cpuz.exe
 }
+
 # create empty sidecar so shimgen creates shim for GUI rather than console
-Set-Content -Path (Join-Path -Path $toolsPath -ChildPath "cpuz.exe.gui") `
-            -Value $null
+Set-Content -Path (Join-Path $toolsPath "cpuz.exe.gui") -Value $null
 
 Write-Host "$packageName installed to $toolsPath"
--- cpu-z.portable/update.ps1
+++ cpu-z.portable/update.ps1
@@ -5,23 +5,24 @@ $releases = 'http://www.cpuid.com/softwares/cpu-z.htm'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-        }
+            "(?i)(^\s*url\s*=\s*)('.*')"       = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"  = "`$1'$($Latest.Checksum32)'"
+            }
     }
 }
 
+
 function global:au_GetLatest {
-    $re      = 'cpu-z.+\.zip'
+    $re  = 'cpu-z.+\.zip'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url     = $download_page.links | ? href -match $re | select -First 1 -Expand href
+    $url = $download_page.links | ? href -match $re | select -First 1 -Expand href
 
     $download_page = Invoke-WebRequest -Uri "http://www.cpuid.com/$url" -UseBasicParsing
-    $url     = $download_page.links | ? href -match $re | select -First 1 -Expand href
+    $url = $download_page.links | ? href -match $re | select -First 1 -Expand href
 
     $current_checksum = (gi $PSScriptRoot\tools\chocolateyInstall.ps1 | sls '^[$]checksum32\b') -split "=|'" | Select -Last 1 -Skip 1
-    if ($current_checksum.Length -ne 64) { throw "Can't find current checksum" }
+    if ($current_checksum.Length -ne 64) { $current_checksum = '0' }
     $remote_checksum  = Get-RemoteChecksum $url
     if ($current_checksum -ne $remote_checksum) {
         Write-Host 'Remote checksum is different then the current one, forcing update'
@@ -38,4 +39,3 @@ function global:au_GetLatest {
 
 update -ChecksumFor none
 if ($global:au_old_force -is [bool]) { $global:au_force = $global:au_old_force }
-
