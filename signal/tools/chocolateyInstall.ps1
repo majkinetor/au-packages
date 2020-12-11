7a980e87ce10303329886c9a9fa2caefd4602532
Chocolatey <chocolatey@realdimensions.net>
2020-12-11 01:02:17 +0000

    AU: 2 updated - rundeck signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/2201fc31e39567a7a141f98e7aa2171f98066a59

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.38.2.exe'
-  checksum               = '24c9605e3034b609fa5cfeaf1725a0faf4787a469527f0d1eb8b712344ce1d4d'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-1.39.2.exe'
+  checksum               = '6698d6de8f5a51eec9cd0cb466765b314aef03fd42fa4bf9c329a356ec3a4e66'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
