b0164ea6d854361dca882a283bc002ca54965577
Miodrag Milic <miodrag.milic@gmail.com>
2021-06-03 18:48:03 +0200

    (influxdb1) update++

--- influxdb1/update.ps1
+++ influxdb1/update.ps1
@@ -16,9 +16,10 @@ function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re      = 'influxdb-[0-9.]+.+_windows_amd64\.zip'
-    $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
-    $version = $url -split '-|_' | ? { try { [version]::Parse($_) } catch {} }
+    $re      = '(?<=wget )https://.+?/influxdb-[0-9.]+_windows_amd64\.zip'
+    $download_page.Content -match $re | Out-Null
+    $url = $Matches[0]
+    $version = $url -split '[-_]' | select -First 1 -Skip 1
 
     @{
         Version = $version
@@ -30,4 +31,3 @@ function global:au_GetLatest {
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
     update -ChecksumFor none
 }
-
