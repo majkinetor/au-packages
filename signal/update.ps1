9cee2f3a5dc071572b822bea3b5e007e40dc186a
Brandon H <bc3tech@users.noreply.github.com>
2021-06-15 09:59:31 -0700

    Allow packaging of betas
    Remove `-notmatch 'beta'` since Signal project uses semver; this will allow beta versions to show up as installable on chocolatey with `--prerelease` functionality.

--- signal/update.ps1
+++ signal/update.ps1
@@ -16,7 +16,7 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $version = $download_page.links.href -match 'tag/v' -notmatch 'beta' | Select -First 1 | % { $_ -split '/' | select -Last 1 }
+    $version = $download_page.links.href -match 'tag/v' | Select -First 1 | % { $_ -split '/' | select -Last 1 }
     $version = $version.Substring(1)
     @{
         Version      = $version
