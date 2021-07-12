dcd91520c2f9cc767a903a08a9e32c02437d4068
Miodrag Milić <miodrag.milic@gmail.com>
2021-07-12 14:48:30 +0200

    (yed) update++

--- yed/update.ps1
+++ yed/update.ps1
@@ -19,15 +19,20 @@ function global:au_BeforeUpdate  { $Latest.Checksum32 = Get-RemoteChecksum $Late
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
-    if ($download_page.RawContent -match 'resources/yed/demo/yEd.{1,10}\.zip')
+    if ($download_page.RawContent -match 'resources/yed/demo/yEd.+?\.exe')
     {
         $url = "https://www.yworks.com/" + $Matches[0]
     } else { throw "Can't find url" }
 
     #$url = "https://www.yworks.com/resources/yed/demo/yEd-3.14.4.zip"
-    $version  = $url -split '[_-]|.zip' | select -Last 1 -Skip 1
+    #https://www.yworks.com/resources/yed/demo/yEd-3.21.1_with-JRE15_64-bit_setup.exe
 
-    @{ URL32 = $url; Version = $version }
+    $version  = $url -split '[_-]|.zip' | select -First 1 -Skip 1
+
+    @{
+        Version = $version
+        URL32   = "https://www.yworks.com/resources/yed/demo/yEd-$version.zip"
+    }
 }
 
 update -ChecksumFor none
