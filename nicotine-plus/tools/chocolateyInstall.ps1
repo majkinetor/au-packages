b194e0dcefc35e86df08cc6556e92595b304c7b2
Miodrag Milić <miodrag.milic@gmail.com>
2021-09-04 22:57:54 +0200

    (nicotine-plus) change sc alias

--- nicotine-plus/tools/chocolateyInstall.ps1
+++ nicotine-plus/tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ $packageArgs = @{
   softwareName   = 'Nicotine+ *'
 }
 Install-ChocolateyInstallPackage @packageArgs
-ls $toolsPath\*\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" "" }}
+ls $toolsPath\*\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" "" }}
 
 $packageName = 'Nicotine+'
 $installLocation = Get-AppInstallLocation "$packageName*"
