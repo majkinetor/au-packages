1f4a6623799874a8b5f94a5fb199488a66eac81c
Chocolatey <chocolatey@realdimensions.net>
2021-10-06 01:09:09 +0000

    AU: 2 updated - plantuml signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/6029175db54ec04cbbe09e786005b9b0a64b5a34

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-5.18.1.exe'
-  checksum               = '979e7a7d6a11db461999125f70e3c4359ea01ee93a5dd5722866695519585a81'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-5.19.0.exe'
+  checksum               = 'ddbc6a9386aff5c7db8008e3befde2bd8ffaff26ee816a8036b4b933398ed410'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
