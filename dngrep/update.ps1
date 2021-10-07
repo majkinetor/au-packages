a786f46a756672b1c4ce70f7de03322d931b1485
Miodrag Milić <miodrag.milic@gmail.com>
2021-10-07 17:23:38 +0200

    (dngrep) add release notes

--- dngrep/update.ps1
+++ dngrep/update.ps1
@@ -8,21 +8,30 @@ function global:au_SearchReplace {
           "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
           "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
           "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+        },
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
         }
      }
 }
 
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing 
-    
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
     $re      = 'dnGREP.*.msi'
     $url     = $download_page.links | ? href -match $re | select -First 2 -expand href
     $url64   = 'https://github.com' + $url[0]
     $url32   = 'https://github.com' + $url[1]
-    $version = ($url[0] -split '\/' | select -Index 5).Substring(1)
+    $version = $url[0] -split '\/' | select -Index 5
 
-    return @{ URL64 = $url64; URL32 = $url32; Version = $version }
+    return @{
+        Version = $version.Substring(1)
+        URL32 = $url32
+        URL64 = $url64
+        ReleaseNotes = "https://github.com/dnGrep/dnGrep/releases/tag/$version"
+    }
 }
 
 update -ChecksumFor none
