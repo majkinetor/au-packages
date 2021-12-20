13eae1dba597319af5431eafa8ad3f2356d2602b
Miodrag Milic <mmilic@knowit.rs>
2021-12-20 10:39:44 +0100

    [AU nicotine-plus:3.2.0] update++

--- nicotine-plus/tools/chocolateyInstall.ps1
+++ nicotine-plus/tools/chocolateyInstall.ps1
@@ -4,20 +4,12 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $Env:ChocolateyPackageName
-  fileType       = 'exe'
-  file           = gi $toolsPath\windows-i686-installer\*.exe
-  file64         = gi $toolsPath\windows-x86_64-installer\*.exe
-  silentArgs     = '/S'
+  fileType       = 'msi'
+  file           = gi $toolsPath\windows-i686-installer\*.msi
+  file64         = gi $toolsPath\windows-x86_64-installer\*.msi
+  silentArgs     = '/q'
   validExitCodes = @(0)
   softwareName   = 'Nicotine+ *'
 }
 Install-ChocolateyInstallPackage @packageArgs
 ls $toolsPath\*\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" "" }}
-
-$packageName = 'Nicotine+'
-$installLocation = Get-AppInstallLocation "$packageName*"
-if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
-Write-Host "$packageName installed to '$installLocation'"
-
-Register-Application "$installLocation\$packageName.exe" -Name nicotine
-Write-Host "$packageName registered as nicotine"
