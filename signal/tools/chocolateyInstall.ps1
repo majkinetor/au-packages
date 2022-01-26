c8d7061ba1cb0de6111dd17aef9c1b4679fcc131
Chocolatey <chocolatey@realdimensions.net>
2022-01-26 01:02:39 +0000

    AU: 1 updated - signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/e33d107139c391da4764f4b63ae78598d89acae6

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-5.29.0.exe'
-  checksum               = '46112c1e71e6d7e33649ced5efb9c034da2c236b72226b0a6f6b823eb5f4a141'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-5.29.1.exe'
+  checksum               = 'b980191fe626a5edd151d898b99a8aa8365a1e0ecabc90d772fc3f792794584c'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
