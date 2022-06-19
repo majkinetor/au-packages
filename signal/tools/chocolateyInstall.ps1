7fb6e0210e101bd673db49e30f27522c386f5a0a
Chocolatey <chocolatey@realdimensions.net>
2022-06-19 01:02:25 +0000

    AU: 2 updated - nirlauncher signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/8921d2e1ff3141c040041c13bbaa6d9177b3385c

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-5.45.1.exe'
-  checksum               = '0cd3b607a439a85965fb180b899ea5ef78b068aae821ea3a4305267aa2cc86ed'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-5.46.0.exe'
+  checksum               = '2d430b988fd2648db81fc47b2513d41a6d4ec901d2f70fad105c4da35c7e3abf'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
