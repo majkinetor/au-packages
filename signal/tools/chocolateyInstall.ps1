313d3e7962ef6c35859b331d28fcba12901ac322
Chocolatey <chocolatey@realdimensions.net>
2022-08-04 01:02:12 +0000

    AU: 3 updated - furmark signal usql [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/98d1180f210d5f1e5b3aaa38ecb1b945e0737a80

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-5.52.0.exe'
-  checksum               = 'aae9a9d5777a05a20a7ae9e59ceb3afbc0172987c4dce3818a24931dfbfbb737'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-5.53.0.exe'
+  checksum               = 'ceef97aa363a0af43d6f53fe383f0b6f7ef596acf1fc0a74653417b9ae241f75'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
