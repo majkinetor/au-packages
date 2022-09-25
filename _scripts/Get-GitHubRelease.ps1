da5b5abb5c94f4d5ebf9ae578593746a1181b7c8
Miodrag Milić <miodrag.milic@gmail.com>
2022-09-25 15:19:26 +0200

    (copyq) update++

--- _scripts/Get-GitHubRelease.ps1
+++ _scripts/Get-GitHubRelease.ps1
@@ -1,6 +1,6 @@
 function Get-GitHubReleaseUrl( $GitHubRepositoryUrl, $Pattern='\.exe$') {
     $latestReleases = "$GitHubRepositoryUrl/releases/latest"
-    $latestPage = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $latestPage = Invoke-WebRequest -Uri $latestReleases -UseBasicParsing
     $latestPage.Content -match '(?<=src=")[^"]+expanded_assets[^"]+' | Out-Null
     $assetsUrl = $Matches[0]
     if (!$assetsUrl) { throw "Can't find assets URL" }
--- copyq/update.ps1
+++ copyq/update.ps1
@@ -1,6 +1,8 @@
 import-module au
+. $PSScriptRoot\..\_scripts\all.ps1
 
-$releases    = 'https://github.com/hluk/CopyQ/releases'
+$GitHubRepositoryUrl = 'https://github.com/hluk/CopyQ'
+
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
@@ -23,18 +25,13 @@ function global:au_SearchReplace {
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-    $re  = "copyq-.*-setup.exe"
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
-    $url = 'https://github.com' + $url
-
-    $version = $url -split '-|.exe' | select -Last 1 -Skip 2
+    $url = Get-GitHubReleaseUrl $GitHubRepositoryUrl
+    $version = $url -split '/' | select -Last 1 -Skip 1
 
     return @{
+        Version      = $version.Substring(1)
         URL32        = $url
-        Version      = $version.Replace('v','')
-        ReleaseNotes = "$releases/tag/${version}"
+        ReleaseNotes = "$GitHubRepositoryUrl/releases/tag/$version"
     }
 }
 
