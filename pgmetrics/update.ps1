b4cabced147dd5fd3ae5bb829e716a411b29bcb3
Miodrag Milić <miodrag.milic@gmail.com>
2022-09-25 15:59:28 +0200

    (pgmetrics) update++

--- pgmetrics/update.ps1
+++ pgmetrics/update.ps1
@@ -1,6 +1,7 @@
 import-module au
+. $PSScriptRoot\..\_scripts\all.ps1
 
-$releases = 'https://github.com/rapidloop/pgmetrics/releases/latest'
+$GitHubRepositoryUrl = 'https://github.com/rapidloop/pgmetrics'
 
 function global:au_SearchReplace {
    @{
@@ -15,26 +16,22 @@ function global:au_SearchReplace {
     }
 }
 
-function global:au_BeforeUpdate { 
-    Get-RemoteFiles -Purge -NoSuffix 
-    
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge -NoSuffix
+
     Set-Alias 7z $Env:chocolateyInstall\tools\7z.exe
     7z e tools\*.zip -otools *.exe -r -y
     rm tools\*.zip -ea 0
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url = Get-GitHubReleaseUrl $GitHubRepositoryUrl 'windows_amd64.zip$'
+    $version = $url -split '/' | select -Last 1 -Skip 1
 
-    $re      = '\.zip$'
-    $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
-    $domain  = $releases -split '(?<=//.+)/' | select -First 1
-    $version = ($url -split '/' | select -Last 1 -Skip 1).Substring(1)
-
-    @{
-        Version      = $version
-        URL32        = $domain + $url
-        ReleaseNotes = "https://github.com/rapidloop/pgmetrics/releases/tag/v${version}"
+    return @{
+        Version      = $version -replace '^v'
+        URL64        = $url
+        ReleaseNotes = "$GitHubRepositoryUrl/releases/tag/$version"
     }
 }
 
