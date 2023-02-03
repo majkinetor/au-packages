6b60442e25fc6d9de2796cd0695396239c692975
Chocolatey <chocolatey@realdimensions.net>
2023-02-03 01:02:38 +0000

    AU: 3 updated - furmark postgrest signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/f0ec686db078f11e171fca5fa7e702d21e30e2af

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.4.0.exe'
-  checksum               = '0a9ba53b999d33d4808b95d2bcf170dde3d1cb4c9d90c5d0b84efd0bf3980ab5'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.4.1.exe'
+  checksum               = 'c5f0c7b7e61f18f59d2e268827420b593abcd414a44f238c2d595429c5d86a77'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
