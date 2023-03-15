0b50d4393aae5718e7c57192f8ab02618c16a311
Chocolatey <chocolatey@realdimensions.net>
2023-03-15 22:37:56 +0000

    AU: 2 updated - ansifilter signal [skip ci] https://gist.github.com/a700c70b8847b29ebb1c918d47ee4eb1/c67bcac20045427d6584bf1a1c9c52bfc1532c5c

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.9.0.exe'
-  checksum               = '423d601ed61e522f01a476d1284fca7001961d7c78fb346cda807764123d6b54'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.10.0.exe'
+  checksum               = 'd4fcb721e282140dd3665157414afc5f0e3f93b0d2a12004f1878ab8ed2260c7'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
