bcd6d3a213a4a4349be132179fc90dd4c2911226
Chocolatey <chocolatey@realdimensions.net>
2023-04-16 01:02:30 +0000

    AU: 2 updated - mls-software-openssh usql [skip ci] https://gist.github.com/majkinetor/a700c70b8847b29ebb1c918d47ee4eb1/f8731241e0ec54e9ae69e7b0dec521be1b849e67

--- mls-software-openssh/tools/chocolateyInstall.ps1
+++ mls-software-openssh/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'mls-software-openssh'
-$url         = 'https://www.mls-software.com/files/setupssh-9.1p1-1.exe'
-$checksum    = 'a1ad16ff2b1bed5a8280b043c98f2dc308dbb4456a39f98f722ee4b14d3df5de'
+$url         = 'https://www.mls-software.com/files/setupssh-9.3p1-1.exe'
+$checksum    = '37e0329a827f72daf157b4e0368b0ead2a81a4dbb04df9d1b6302d0f6e9f4d61'
 
 $pp = Get-PackageParameters
 $params = @()
