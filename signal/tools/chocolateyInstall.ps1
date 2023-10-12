1835a1fe5b215e1453143add940f797f19ef03b1
Chocolatey <chocolatey@realdimensions.net>
2023-10-12 19:21:05 +0000

    AU: 3 updated - signal trid ximple [skip ci] https://gist.github.com/majkinetor/a700c70b8847b29ebb1c918d47ee4eb1/06390ab4a211ff9785038c7b68aecc68046358af

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.33.0.exe'
-  checksum               = '340b8265ac7992cc40f3cc90a2a71805e10f036b73cf82c730fccb2047549c29'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.34.0.exe'
+  checksum               = '4c7dc19c4cf30f9a9eb30c274a84334b8fe03b50d7a39ea282faa049470716f9'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
