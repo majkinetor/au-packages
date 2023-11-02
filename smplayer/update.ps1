c54def2835cf2e8306fbd823574cf57587b5757c
Miodrag Milic <miodrag.milic@gmail.com>
2023-11-03 00:18:32 +0100

    (smplayer) update++

--- smplayer/update.ps1
+++ smplayer/update.ps1
@@ -1,6 +1,7 @@
 import-module au
+. $PSScriptRoot\..\_scripts\all.ps1
 
-$releases = 'https://sourceforge.net/projects/smplayer/files/SMPlayer'
+$GitHubRepositoryUrl = "https://github.com/smplayer-dev/smplayer"
 
 function global:au_SearchReplace {
     @{
@@ -10,39 +11,20 @@ function global:au_SearchReplace {
         }
 
         ".\legal\verification.txt" = @{
-          "(?i)(\s+x32:).*"        = "`${1} $($Latest.URL32)"
           "(?i)(\s+x64:).*"        = "`${1} $($Latest.URL64)"
-          "(?i)(checksum32:).*"    = "`${1} $($Latest.Checksum32)"
           "(?i)(checksum64:).*"    = "`${1} $($Latest.Checksum64)"
         }
     }
 }
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameSkip 1 -NoSuffix }
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url = $download_page.links | ? {
-        if (!$_.href) { return }
-        $v = $_.href.split('/', [System.StringSplitOptions]::RemoveEmptyEntries)[-1]
-        [version]::TryParse($v, [ref]($__)) } | select -First 1 -Expand href
-
-    $version = $url.split('/', [System.StringSplitOptions]::RemoveEmptyEntries)[-1]
-
-    $releases = 'https://sourceforge.net' + $url
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url32 = $download_page.links | ? href -match '-win32-qt5.6.exe/download' | % href
-    $url64 = $download_page.links | ? href -match '-x64\.exe/download' | % href
-    if (!$url32 -and !$url64) {
-        Write-Host 'No Windows binaries found'
-        return 'ignore'
-    }
-
+    $url = Get-GitHubReleaseUrl $GitHubRepositoryUrl 'smplayer-.+-x64-unsigned\.exe$' | select -Last 1
+    $version = $url -split '/' | select -Last 1 -Skip 1
     @{
-        Version = $version
-        URL32   = $url32
-        URL64   = $url64
-        FileType = 'exe'
+        Version = $version.Substring(1)
+        URL64   = $url
     }
 }
 
