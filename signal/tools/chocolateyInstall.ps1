31b0d1224e62fdd68f98cd40efabdb3e236c511e
Chocolatey <chocolatey@realdimensions.net>
2023-12-01 01:02:23 +0000

    AU: 4 updated - 1remote signal trid usql [skip ci] https://gist.github.com/majkinetor/a700c70b8847b29ebb1c918d47ee4eb1/bc1a38ad7c3d48ab5e5a8fb3f6187e4a4b58e922

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.39.1.exe'
-  checksum               = '77060f5d8923f41a1146575475c858ee009e1f60cea555e638e97b00291afd88'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.40.0.exe'
+  checksum               = '0f613e809b9c0aa5d81113360bc47349838e928087116ac0dff6f853d439aa8d'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
