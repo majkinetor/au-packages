26d30183c3985fd58c733df609fca145575d73b2
Chocolatey <chocolatey@realdimensions.net>
2023-12-08 01:03:10 +0000

    AU: 3 updated - 1remote signal trid [skip ci] https://gist.github.com/majkinetor/a700c70b8847b29ebb1c918d47ee4eb1/18ecf2535fee16a5fec3102b895a678ed88202b0

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.40.0.exe'
-  checksum               = '0f613e809b9c0aa5d81113360bc47349838e928087116ac0dff6f853d439aa8d'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.41.0.exe'
+  checksum               = 'bd737c830458c54488f19ac857a0203ccd1fd2f42851a985118fd17fc484f2a5'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
