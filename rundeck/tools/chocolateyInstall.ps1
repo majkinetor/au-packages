1ea98e1ca8ad62a04bcfdc72c9659c08f2edf46a
Chocolatey <chocolatey@realdimensions.net>
2023-12-16 01:02:55 +0000

    AU: 3 updated - invoke-build rundeck trid [skip ci] https://gist.github.com/majkinetor/a700c70b8847b29ebb1c918d47ee4eb1/fb743a141fb4f107cf8d844945f913b7e759008b

--- rundeck/tools/chocolateyInstall.ps1
+++ rundeck/tools/chocolateyInstall.ps1
@@ -11,8 +11,8 @@ if (!$pp.InstallDir) { $pp.InstallDir = 'C:\rundeck' }
 Write-Host "Installing to" $pp.InstallDir
 mkdir $pp.InstallDir -ea 0 | Out-Null
 
-$checksum32 = '4543da316ea27656ccc155bfc4eaf7d2c8b507d1d43ed82c72dbb191f4da991c'
-$url = 'https://packagecloud.io/pagerduty/rundeck/packages/java/org.rundeck/rundeck-4.17.3-20231113.war/artifacts/rundeck-4.17.3-20231113.war/download'
+$checksum32 = '97e5aac5739c0438c6bd433c78bcdd10f8e7293d5f99ccebe13e850aa134ce56'
+$url = 'https://packagecloud.io/pagerduty/rundeck/packages/java/org.rundeck/rundeck-5.0.0-20231216.war/artifacts/rundeck-5.0.0-20231216.war/download'
 Get-ChocolateyWebFile rundeck "$($pp.InstallDir)\rundeck.war" $url -Checksum $checksum32
 
 Write-Host "Setting up machine environment variable RDECK_BASE"
