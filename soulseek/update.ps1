e1e7930d5843f617926856379b67d854560c6b92
Miodrag Milic <miodrag.milic@gmail.com>
2024-02-18 15:50:55 +0100

    (soulseek) update++

--- soulseek/update.ps1
+++ soulseek/update.ps1
@@ -5,8 +5,8 @@ $changelog = 'http://www.soulseekqt.net/news/node/1'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')" = "`$1'$($Latest.URL)'" 
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^\s+url64bit\s*=\s*)('.*')" = "`$1'$($Latest.URL)'"
+            "(^\s+checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -14,7 +14,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $changelog
 
-    $url     = $download_page.links | ? href -like '*.exe*' | select -First 1 -expand href
+    $url     = $download_page.links | ? href -like '*64bit.exe*' | select -First 1 -expand href
     $version = $url -split '-|\.' | select -Last 3 -Skip 2
     $version = $version -join '.'
     $url     = $url -replace '\?.+'
@@ -22,4 +22,4 @@ function global:au_GetLatest {
     return @{ URL = $url; Version = $version }
 }
 
-update -NoCheckUrl -ChecksumFor 32
+update -NoCheckUrl -ChecksumFor 64 -Force
