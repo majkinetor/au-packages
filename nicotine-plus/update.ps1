35a72cb91cfb42a6fc07d9334e98b9a4c5da5b04
Miodrag Milic <miodrag.milic@gmail.com>
2024-02-18 15:31:47 +0100

    [AU nicotine-plus:3.3.0] fixes

--- nicotine-plus/update.ps1
+++ nicotine-plus/update.ps1
@@ -1,4 +1,6 @@
-$releases = 'https://github.com/nicotine-plus/nicotine-plus/releases'
+. $PSScriptRoot\..\_scripts\all.ps1
+
+$GitHubRepositoryUrl = 'https://github.com/nicotine-plus/nicotine-plus'
 
 function global:au_SearchReplace {
    @{
@@ -7,9 +9,7 @@ function global:au_SearchReplace {
         }
 
         ".\legal\VERIFICATION.txt" = @{
-          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
           "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
-          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
           "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
         }
     }
@@ -24,17 +24,12 @@ function global:au_BeforeUpdate {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $domain  = $releases -split '(?<=//.+)/' | select -First 1
-
-    $re      = 'windows-.+?installer\.zip$'
-    $url     = $download_page.links | ? href -match $re | select -First 2 -expand href | % { $domain + $_}
-    $version = $url[0] -split '/' | select -Last 1 -Skip 1
+    $url = Get-GitHubReleaseUrl $GitHubRepositoryUrl 'windows-.+?installer\.zip$'
+    $version = $url -split '/' | select -Last 1 -Skip 1
 
     @{
         Version      = $version
-        URL32        = $url -match 'i686'    | select -First 1
-        URL64        = $url -notmatch 'i686' | select -First 1
+        URL64        = $url
         ReleaseNotes = "https://github.com/nicotine-plus/nicotine-plus/releases/tag/$version"
     }
 }
