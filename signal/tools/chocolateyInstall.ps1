60eb1cd0ce9cabbec9844741cac541ee1856928c
Chocolatey <chocolatey@realdimensions.net>
2024-02-23 01:02:32 +0000

    AU: 3 updated - freerdp.portable signal trid [skip ci] https://gist.github.com/majkinetor/a700c70b8847b29ebb1c918d47ee4eb1/5a782c27390c169536de30eb4c9fe91101cb5b01

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.48.0.exe'
-  checksum               = 'e7b63669b5809282aa81e598fb0744910ea1c36efa35a298b3c1211c159e200d'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-6.48.1.exe'
+  checksum               = '52edb49b2d92fbb4ff82981fc618d33dde12a28c277ce97d21b2abe825f2e3e7'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
