94b65f277d93094215ce560fe34aee119476aeb8
Chocolatey <chocolatey@realdimensions.net>
2024-04-26 01:03:11 +0000

    AU: 2 updated - signal trid [skip ci] https://gist.github.com/majkinetor/a700c70b8847b29ebb1c918d47ee4eb1/aae1521a03117366c0ccb92dd9c67e468b9130be

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-7.5.1.exe'
-  checksum               = '6f367190f8165abcb5baef9b2ca5dae432412d77b03d4ebffcb9a9787431fb2b'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-7.6.0.exe'
+  checksum               = '39df24d352aa227fed22dab53b8a455915237c300673433810d8a9f4794f739b'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
