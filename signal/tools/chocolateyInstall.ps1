60c419a3e34e13239d69248f86706a9d058850ad
Chocolatey <chocolatey@realdimensions.net>
2024-06-21 01:03:05 +0000

    AU: 3 updated - gitlab-runner signal visualsvnserver [skip ci] https://gist.github.com/majkinetor/a700c70b8847b29ebb1c918d47ee4eb1/32b998cb188a6a31ae97a63b44dc01ff56380563

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-7.12.0.exe'
-  checksum               = 'e1956845857b7cb507359087f837a60cca0febbe5b0b3ef718ccfe15ee9ba560'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-7.13.0.exe'
+  checksum               = '33bc15b97bab08805e845ec108723a892c679430b47badc3b27989ff597075df'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
