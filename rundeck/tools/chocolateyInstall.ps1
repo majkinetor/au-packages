d06de18d47fd50d0e31b46049a3288c6101ca4e8
Chocolatey <chocolatey@realdimensions.net>
2024-08-09 01:03:11 +0000

    AU: 5 updated - bruno freerdp.portable loki rundeck trid [skip ci] https://gist.github.com/majkinetor/a700c70b8847b29ebb1c918d47ee4eb1/5948c85948232e75d594b066f9895914a6175271

--- rundeck/tools/chocolateyInstall.ps1
+++ rundeck/tools/chocolateyInstall.ps1
@@ -11,8 +11,8 @@ if (!$pp.InstallDir) { $pp.InstallDir = 'C:\rundeck' }
 Write-Host "Installing to" $pp.InstallDir
 mkdir $pp.InstallDir -ea 0 | Out-Null
 
-$checksum32 = '38a71d19c271a5b9153b5e840e6a14a8c2ee3bd79564466b5ccc28c1ad683f2f'
-$url = 'https://packagecloud.io/pagerduty/rundeck/packages/java/org.rundeck/rundeck-5.4.0-20240618.war/artifacts/rundeck-5.4.0-20240618.war/download'
+$checksum32 = '32397822e44012d5559f77ed9f1907de3b73e9d11289ba1332ae2322880eaa01'
+$url = 'https://packagecloud.io/pagerduty/rundeck/packages/java/org.rundeck/rundeck-5.5.0-20240808.war/artifacts/rundeck-5.5.0-20240808.war/download'
 Get-ChocolateyWebFile rundeck "$($pp.InstallDir)\rundeck.war" $url -Checksum $checksum32
 
 Write-Host "Setting up machine environment variable RDECK_BASE"
