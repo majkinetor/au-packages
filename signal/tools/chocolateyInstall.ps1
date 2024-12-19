d2a0244cb97d36919d983f9b95619a30c431820d
Chocolatey <chocolatey@realdimensions.net>
2024-12-19 03:17:37 +0000

    AU: 2 updated - 1remote signal [skip ci] https://gist.github.com/majkinetor/a700c70b8847b29ebb1c918d47ee4eb1/b3ed48726d84c38b2412efa127a9a23015e1c825

--- signal/tools/chocolateyInstall.ps1
+++ signal/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ Set-AutoUpdate -Enable
 $packageArgs = @{
   packageName            = 'signal'
   fileType               = 'EXE'
-  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-7.36.0.exe'
-  checksum               = '76d664fd84a02423479a67bc96274e03824d9485f6ef6e04e552481d129951bd'
+  url                    = 'https://updates.signal.org/desktop/signal-desktop-win-7.36.1.exe'
+  checksum               = '84f97082b394cac8c308436dba391e236dce897740279824d622b79d20121f16'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
